6402a9ebbeab065f0f9f9c274c1ee15a4b34d987
Sigurdur Asgeirsson <siggi@chromium.org>
2010-01-14 15:02:19 +0000

    Add a config constants file missing from previous checkin.
    Review URL: http://codereview.appspot.com/183148
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@19 15e8cca8-e42c-11de-a347-f34a4f72eb7d

---
+++ sawbuck/viewer/const_config.h
@@ -0,0 +1,31 @@
+// Copyright 2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Configuration-related constants.
+#ifndef SAWBUCK_VIEWER_CONST_CONFIG_H_
+#define SAWBUCK_VIEWER_CONST_CONFIG_H_
+
+namespace config {
+
+const wchar_t kSettingsKey[] = L"Software\\Google\\SawBuck";
+const wchar_t kProviderNamesKey[] = L"Software\\Google\\SawBuck\\Providers";
+const wchar_t kProviderLevelsKey[] = L"Software\\Google\\SawBuck\\Levels";
+
+const wchar_t kWindowPosValue[] = L"window_pos";
+const wchar_t kLogViewColumnOrder[] = L"log_view_column_order";
+const wchar_t kLogViewColumnWidths[] = L"log_view_column_widths";
+
+}  // namespace config
+
+#endif  // SAWBUCK_VIEWER_CONST_CONFIG_H_
