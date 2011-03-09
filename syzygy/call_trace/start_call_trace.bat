e5f54530297b6c3003ef0d342eb65f05d308759b
Sigurdur Asgeirsson <siggi@chromium.org>
2011-03-09 17:05:30 +0000

    Revert files accidentally committed with previous CL.
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@199 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- syzygy/call_trace/start_call_trace.bat
+++ syzygy/call_trace/start_call_trace.bat
@@ -22,11 +22,7 @@ set clock_type=
 
 :main
 :: Create a kernel logger session and make it log image events to the file kernel.etl.
-:: "hf" is to enable hard page fault logging.
-:: "pf" is all other page fault logging.
-:: "file" is to log file object -> name mappings through "Name" & "Rundown" events
-:: "fileio" is to log file io events, e.g. creates, deletes, reads, writes.
-logman create trace -ets "NT Kernel Logger" %clock_type% -mode globalsequence -bs 10240 -nb 25 50 -o kernel.etl -p "Windows Kernel Trace" (img,process,thread,pf,hf,file,fileio)
+logman create trace -ets "NT Kernel Logger" %clock_type% -mode globalsequence -bs 10240 -nb 25 50 -o kernel.etl -p "Windows Kernel Trace" (img,process,thread,pf,hf)
 
 :: Create the call trace logger session.
 logman create trace -ets "call_trace" %clock_type% -mode globalsequence -bs 10240 -nb 25 50 -o call_trace.etl
