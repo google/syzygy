1bfb1d72269fcda16dcd62940e0f0cafd5bbff49
Sigurdur Asgeirsson <siggi@chromium.org>
2011-06-10 15:01:44 +0000

    SSE3 variant of version 1.6.0 of numpy for Python 2.6. Review URL: http://codereview.appspot.com/4585048
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@340 15e8cca8-e42c-11de-a347-f34a4f72eb7d

---
+++ third_party/numpy/files/numpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+short_version = '1.6.0'
+version = '1.6.0'
+full_version = '1.6.0'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
