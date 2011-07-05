4c976bd8490b3e65173146b5b73592400675959a
chrisha@chromium.org <chrisha@chromium.org@15e8cca8-e42c-11de-a347-f34a4f72eb7d>
2011-07-05 16:40:20 +0000

    Revamped linear order generator
    Extended the reorderer base class to parse process information, allowing it to
    distinguish between multiple runs of an instrumented module in a single ETL
    file, as well as multiple runs across multiple ETL files. Extended
    LinearOrderGenerator to use this information. The generalized behaviour of
    LinearOrderGenerator is to group function calls by popularity, and linearize
    their order of access. This is equivalent to the previous behaviour for cases
    where there is only a single run of the instrumented module.
    Review URL: http://codereview.appspot.com/4641092
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk/syzygy@375 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- reorder/linear_order_generator.cc
+++ reorder/linear_order_generator.cc
@@ -17,11 +17,95 @@
 
 namespace reorder {
 
+namespace {
+
+// This is effectively arbitrarily chosen for now. Higher is better from
+// benchmarked reorderings. This value is effectively 'infinite' as far as the
+// chrome data goes.
+const size_t kDataRecursionDepth = 100;
+
+typedef LinearOrderGenerator::BlockCall BlockCall;
+
+// Comparator for sorting BlockCalls by increasing time.
+struct BlockCallSortIncrTime {
+  bool operator()(const BlockCall& bc1, const BlockCall& bc2) {
+    return bc1.time < bc2.time;
+  }
+};
+
+// Used for aggregating block call information across multiple runs.
+struct AverageBlockCall {
+  const BlockGraph::Block* block;
+  size_t sum_order;
+  // The number of runs of the instrumented binary in which this block was
+  // seen, NOT the number of times it was seen called in aggregate.
+  size_t call_count;
+  // This is only meaningful if call_count == 1.
+  uint32_t process_group_id;
+
+  double AverageOrder() const {
+    DCHECK(call_count > 0);
+    return ((double)sum_order) / call_count;
+  }
+};
+
+// Sorts by decreasing call count. Anything with more than one call count
+// is sorted with a secondary key of increasing order. Anything with a single
+// call count has a secondary key of process_group_id, and a tertiary key of
+// increasing order.
+struct AverageBlockCallSort {
+  bool operator()(const AverageBlockCall& abc1, const AverageBlockCall& abc2) {
+    if (abc1.call_count != abc2.call_count)
+      return abc1.call_count > abc2.call_count;
+
+    if (abc1.call_count > 1 || abc1.process_group_id == abc2.process_group_id)
+      return abc1.AverageOrder() < abc2.AverageOrder();
+
+    return abc1.process_group_id < abc2.process_group_id;
+  }
+};
+
+// Extract the values of a map to a vector.
+template<typename K, typename V> void MapToVector(const std::map<K, V> map,
+                                                  std::vector<V>* vector) {
+  DCHECK(vector != NULL);
+
+  vector->clear();
+  std::map<K, V>::const_iterator it = map.begin();
+  for (; it != map.end(); ++it)
+    vector->push_back(it->second);
+}
+
+}  // namespace
+
 LinearOrderGenerator::LinearOrderGenerator()
-    : Reorderer::OrderGenerator("Linear Order Generator") {
+    : Reorderer::OrderGenerator("Linear Order Generator"),
+      active_process_count_(0),
+      next_process_group_id_(0) {
 }
 
 LinearOrderGenerator::~LinearOrderGenerator() {
+}
+
+bool LinearOrderGenerator::OnProcessStarted(const Reorderer& reorderer,
+                                            uint32 process_id,
+                                            const UniqueTime& time) {
+  if (active_process_count_ == 0) {
+    if (!CloseProcessGroup())
+      return false;
+  }
+
+  ++active_process_count_;
+
+  return true;
+}
+
+bool LinearOrderGenerator::OnProcessEnded(const Reorderer& reorderer,
+                                          uint32 process_id,
+                                          const UniqueTime& time) {
+  DCHECK_LT(0U, active_process_count_);
+  --active_process_count_;
+  return true;
 }
 
 bool LinearOrderGenerator::OnCodeBlockEntry(const Reorderer& reorderer,
@@ -33,98 +117,149 @@ bool LinearOrderGenerator::OnCodeBlockEntry(const Reorderer& reorderer,
   if (!reorderer.MustReorder(block))
     return true;
 
-  return TouchBlock(block, time);
+  return TouchBlock(BlockCall(block, process_id, thread_id, time));
 }
 
 bool LinearOrderGenerator::CalculateReordering(const Reorderer& reorderer,
                                                Order* order) {
   DCHECK(order != NULL);
 
-  // If data ordering is enabled, turn each code block event into a set
-  // of data block events as well. This creates new blocks in the map as we go,
-  // so we need to filter on code blocks only.
-  if (reorderer.flags() & Reorderer::kFlagReorderData) {
-    BlockCallMap::const_iterator it = block_calls_.begin();
-    for (; it != block_calls_.end(); ++it) {
-      DCHECK(it->first != NULL);
-      if (it->first->type() == BlockGraph::CODE_BLOCK &&
-          !TouchDataBlocks(reorderer, it->first, it->second))
+  // Ensure the last running process group is closed.
+  if (!CloseProcessGroup())
+    return false;
+
+  LOG(INFO) << "Encountered " << process_group_calls_.size()
+      << " process groups.";
+
+  // Aggregate the block calls.
+  std::map<const BlockGraph::Block*, AverageBlockCall> average_block_call_map;
+  ProcessGroupBlockCalls::const_iterator it = process_group_calls_.begin();
+  for (; it != process_group_calls_.end(); ++it) {
+    for (size_t i = 0; i < it->second.size(); ++i) {
+      const BlockCall& block_call = it->second[i];
+      AverageBlockCall& average_block_call =
+          average_block_call_map[block_call.block];
+      average_block_call.block = block_call.block;
+      average_block_call.sum_order += i;
+      ++average_block_call.call_count;
+      average_block_call.process_group_id = it->first;
+    }
+  }
+
+  // Now create a sorted list.
+  std::vector<AverageBlockCall> average_block_calls;
+  MapToVector(average_block_call_map, &average_block_calls);
+  average_block_call_map.clear();
+  std::sort(average_block_calls.begin(), average_block_calls.end(),
+            AverageBlockCallSort());
+
+  // TODO(chrisha): Create an option to evenly distribute the common startup
+  //     blocks (those with call_count == process_group_calls_.size()) among
+  //     the remaining blocks.
+
+  // Create the ordering from this list.
+  for (size_t i = 0; i < average_block_calls.size(); ++i) {
+    const BlockGraph::Block* code_block = average_block_calls[i].block;
+
+    if (reorderer.MustReorder(code_block))
+      order->section_block_lists[code_block->section()].push_back(code_block);
+
+    // Create an anologous data ordering if we were asked to.
+    if (reorderer.flags() & Reorderer::kFlagReorderData) {
+      if (!InsertDataBlocks(kDataRecursionDepth, reorderer, code_block, order))
         return false;
     }
   }
 
-  typedef std::vector<std::pair<UniqueTime, const BlockGraph::Block*> >
-      BlockCallVector;
-
-  // Turn the BlockCallMap into a BlockCallVector, and sort based on time.
-  BlockCallVector calls(block_calls_.size());
-  BlockCallMap::const_iterator it = block_calls_.begin();
-  for (size_t i = 0; it != block_calls_.end(); ++it, ++i) {
-    calls[i].first = it->second;
-    calls[i].second = it->first;
-  }
-  std::sort(calls.begin(), calls.end());
-
-  // Create the output, which is simply the new ordering of blocks, per section.
-  // We currently throw away any blocks that map to an invalid section id.
-  // TODO(chrisha): We need to make sure that all blocks in the decomposed
-  //     image properly set the 'section' attribute of Block.
-  for (size_t i = 0; i < calls.size(); ++i) {
-    const BlockGraph::Block* block = calls[i].second;
-    size_t section_id = block->section();
-    if (section_id == core::kInvalidSection)
-      continue;
-    if (!reorderer.MustReorder(section_id))
-      continue;
-    order->section_block_lists[section_id].push_back(block);
-  }
-
   return true;
 }
 
-bool LinearOrderGenerator::TouchBlock(const BlockGraph::Block* block,
-                                      const UniqueTime& time) {
+bool LinearOrderGenerator::TouchBlock(const BlockCall& block_call) {
+  DCHECK(block_call.block != NULL);
+  // All code blocks should belong to a defined section.
+  DCHECK_NE(pe::kInvalidSection, block_call.block->section());
+
   // Store the block along with the earliest time it was called.
-  BlockCallMap::iterator it = block_calls_.find(block);
-  if (it == block_calls_.end()) {
+  BlockCallMap::iterator it = block_call_map_.find(block_call.block);
+  if (it == block_call_map_.end()) {
     std::pair<BlockCallMap::iterator, bool> insert_return;
-    insert_return = block_calls_.insert(std::make_pair(block, time));
+    insert_return = block_call_map_.insert(
+        std::make_pair(block_call.block, block_call));
     it = insert_return.first;
     DCHECK(insert_return.second);
-    DCHECK(it != block_calls_.end());
+    DCHECK(it != block_call_map_.end());
   } else {
     // Keep around the earliest call to this block only.
-    it->second = std::min(it->second, time);
+    if (block_call.time < it->second.time)
+      it->second = block_call;
   }
   return true;
 }
 
-// Given a code block, touches the data blocks associated with it.
-bool LinearOrderGenerator::TouchDataBlocks(const Reorderer& reorderer,
-                                           const BlockGraph::Block* code_block,
-                                           const UniqueTime& time) {
-  DCHECK(code_block != NULL);
-  DCHECK(code_block->type() == BlockGraph::CODE_BLOCK);
+bool LinearOrderGenerator::InsertDataBlocks(size_t max_recursion_depth,
+                                            const Reorderer& reorderer,
+                                            const BlockGraph::Block* block,
+                                            Order* order) {
+  DCHECK(block != NULL);
+  DCHECK(order != NULL);
+
+  // Stop the recursion.
+  if (max_recursion_depth == 0)
+    return true;
+
+  std::vector<const BlockGraph::Block*> data_blocks;
 
   // Iterate through any data blocks that are referenced by this
-  // function, and also store them with the same time. This is a pessimistic
+  // block, and also store them with the same time. This is a pessimistic
   // optimization, and assumes that all data linked to a code block will
-  // be touched by that code block.
+  // be touched by that code block (and all data linked to by that data block,
+  // and so on, up to 'max_recursion_depth').
   BlockGraph::Block::ReferenceMap::const_iterator ref_it =
-      code_block->references().begin();
-  for (; ref_it != code_block->references().end(); ++ref_it) {
+      block->references().begin();
+  for (; ref_it != block->references().end(); ++ref_it) {
     const BlockGraph::Block* ref = ref_it->second.referenced();
     DCHECK(ref != NULL);
-    // Only update non-code blocks, as we get all code-block events directly
-    // from the trace.
-    if (ref->type() == BlockGraph::CODE_BLOCK)
-      continue;
-    if (!reorderer.MustReorder(ref))
+    // We only touch data blocks with a valid section id.
+    if (ref->type() != BlockGraph::DATA_BLOCK ||
+        ref->section() == pe::kInvalidSection)
       continue;
 
-    if (!TouchBlock(ref, time))
-      return false;
+    // Only insert data blocks that have not yet been seen.
+    if (!data_block_set_.insert(ref).second)
+      continue;
+
+    // Finally, insert this block to the appropriate section ordering, but
+    // only if we're reordering that section.
+    if (reorderer.MustReorder(ref))
+      order->section_block_lists[ref->section()].push_back(ref);
+
+    data_blocks.push_back(ref);
   }
+
+  // Recurse on the data blocks we just added.
+  if (max_recursion_depth > 1) {
+    for (size_t i = 0; i < data_blocks.size(); ++i) {
+      if (!InsertDataBlocks(max_recursion_depth - 1, reorderer,
+                            data_blocks[i], order))
+        return false;
+    }
+  }
+
+  return true;
+}
+
+bool LinearOrderGenerator::CloseProcessGroup() {
+  if (block_call_map_.size() == 0)
+    return true;
+
+  BlockCalls& block_calls = process_group_calls_[next_process_group_id_];
+  ++next_process_group_id_;
+
+  MapToVector(block_call_map_, &block_calls);
+  block_call_map_.clear();
+
+  std::sort(block_calls.begin(), block_calls.end(), BlockCallSortIncrTime());
+
   return true;
 }
 
--- reorder/linear_order_generator.h
+++ reorder/linear_order_generator.h
@@ -17,6 +17,25 @@
 // If data ordering is enabled, all data blocks referred to by a code block
 // are assumed to have been touched when the code block was executed, and they
 // are output in that order.
+//
+// If multiple runs of the instrumented binary are seen in the trace files, each
+// run will be processed independently, and for each unique block, the count of
+// how many runs in which its execution was seen is maintained. Blocks are first
+// sorted by the number of individual runs in which they were seen (decreasing),
+// and then sorted by the order in which they were seen. For the special case
+// of blocks that were only seen in a single run of the binary, these are
+// separated by run and then sorted by time seen.
+//
+// Consider a trace file that contains 3 runs of an executable. The generated
+// ordering will be as follows:
+//
+// code seen in all 3 runs, code seen in any 2 runs, code seen only in 1st run,
+//     code seen only in 2nd run, code seen only in 3rd run
+//
+// In the case where there is a single run of the instrumented binary, the
+// ordering will be a simple ordering of blocks by order of execution, as per
+// our original proof-of-concept ordering.
+
 #ifndef SYZYGY_REORDER_LINEAR_ORDER_GENERATOR_H_
 #define SYZYGY_REORDER_LINEAR_ORDER_GENERATOR_H_
 
@@ -30,11 +49,18 @@ class LinearOrderGenerator : public Reorderer::OrderGenerator {
  public:
   typedef Reorderer::UniqueTime UniqueTime;
   typedef Reorderer::Order Order;
+  struct BlockCall;
 
   LinearOrderGenerator();
   virtual ~LinearOrderGenerator();
 
   // OrderGenerator implementation.
+  virtual bool OnProcessStarted(const Reorderer& reorderer,
+                                uint32 process_id,
+                                const UniqueTime& time);
+  virtual bool OnProcessEnded(const Reorderer& reorderer,
+                              uint32 process_id,
+                              const UniqueTime& time);
   virtual bool OnCodeBlockEntry(const Reorderer& reorderer,
                                 const BlockGraph::Block* block,
                                 RelativeAddress address,
@@ -45,17 +71,57 @@ class LinearOrderGenerator : public Reorderer::OrderGenerator {
                                    Order* order);
 
  private:
-  typedef std::map<const BlockGraph::Block*, UniqueTime> BlockCallMap;
+  typedef std::vector<BlockCall> BlockCalls;
+  typedef std::map<size_t, BlockCalls> ProcessGroupBlockCalls;
+  typedef std::map<const BlockGraph::Block*, BlockCall> BlockCallMap;
 
   // Called by OnFunctionEntry to update block_calls_.
-  bool TouchBlock(const BlockGraph::Block* block, const UniqueTime& time);
-  // Given a code block, touches the data blocks associated with it.
-  bool TouchDataBlocks(const Reorderer& reorderer,
-                       const BlockGraph::Block* code_block,
-                       const UniqueTime& time);
+  bool TouchBlock(const BlockCall& block_call);
+
+  // Given a block, inserts the data blocks associated with it into
+  // the ordering. Will recursively traverse data blocks until the given
+  // maximum stack depth (that way, we include data referred to by data).
+  bool InsertDataBlocks(size_t max_recursion_depth,
+                        const Reorderer& reorderer,
+                        const BlockGraph::Block* block,
+                        Order* order);
+
+  // This is called to indicate a process group closure.
+  bool CloseProcessGroup();
+
+  // We assume that processes that co-exist are all part of a single run.
+  // So we divide up block calls per run. This counts the number of currently
+  // active processes, and when it reaches zero it means that we need to
+  // start a new process.
+  size_t active_process_count_;
+
+  // This is for creating unique ids for groups of coexisting processes.
+  // This is incremented every time active_process_count transitions from 0
+  // to 1.
+  size_t next_process_group_id_;
+
+  // Stores the linearized block list per process group.
+  ProcessGroupBlockCalls process_group_calls_;
 
   // Stores pointers to blocks, and the first time at which they were accessed.
-  BlockCallMap block_calls_;
+  // There is one of these per 'process group'.
+  BlockCallMap block_call_map_;
+
+  // Stores a list of already-inserted data blocks.
+  std::set<const BlockGraph::Block*> data_block_set_;
+};
+
+struct LinearOrderGenerator::BlockCall {
+  const BlockGraph::Block* block;
+  uint32_t process_id;
+  uint32_t thread_id;
+  UniqueTime time;
+
+  BlockCall(const BlockGraph::Block* block, uint32_t process_id,
+            uint32_t thread_id, const UniqueTime& time)
+      : block(block), process_id(process_id), thread_id(thread_id),
+        time(time) {
+  }
 };
 
 }  // namespace reorder
