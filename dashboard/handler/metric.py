22ab3af530289e3918f050c6439b0ade49441c78
ericdingle@chromium.org <ericdingle@chromium.org@15e8cca8-e42c-11de-a347-f34a4f72eb7d>
2011-12-22 18:11:19 +0000

    Implement dashboard datum request handler.
    BUG=
    TEST=
    
    Review URL: http://codereview.appspot.com/5504061
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk/syzygy@585 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- dashboard/handler/metric.py
+++ dashboard/handler/metric.py
@@ -22,18 +22,17 @@ class MetricHandler(webapp.RequestHandler):
   """A class to handle creating and querying metrics."""
 
   def get(self, client_id, metric_id):
-    """Responds with information about clients.
+    """Responds with information about metrics.
 
-    If no metric_id is specified, a list of available metrics for the client
-    is returned. If a metric_id is specified, information about that metric
-    is returned.
+    If the client does not exist, responds with a 404. If a metric_id is not
+    specified, responds with a JSON encoded list of available metrics for the
+    client. If a metric_id is specified, responds with JSON encoded
+    information about the metric or a 404 if the metric doesn't exist.
 
     Args:
       client_id: The client ID.
       metric_id: The metric ID. Maybe be an empty string.
     """
-    self.response.headers['Content-Type'] = 'application/json'
-
     client = client_db.Client.get_by_key_name(client_id)
     if not client:
       self.error(404)  # Not found.
@@ -45,31 +44,37 @@ class MetricHandler(webapp.RequestHandler):
         self.error(404)  # Not found.
         return
 
-      data = {'id': metric.key().id_or_name(),
-              'description': metric.description,
-              'units': metric.units}
+      result = {'id': metric.key().id_or_name(),
+                'description': metric.description,
+                'units': metric.units}
     else:
       metrics = metric_db.Metric.all()
       metrics.ancestor(client)
 
-      data = []
+      result = []
       for metric in metrics:
-        data.append({'id': metric.key().id_or_name(),
-                     'description': metric.description,
-                     'units': metric.units})
+        result.append({'id': metric.key().id_or_name(),
+                       'description': metric.description,
+                       'units': metric.units})
 
-    json.dump(data, self.response.out)
+    self.response.headers['Content-Type'] = 'application/json'
+    json.dump(result, self.response.out)
 
-  def post(self, client_id, _):
+  def post(self, client_id, metric_id):
     """Creates a new metric.
 
     Adds a metric to the data store. The ID, description and units should be
-    specified in the body of the request. The response is 'OK' on success or
-    an error message on failure.
+    specified as POST parameters in the request. Responds with a 200 on success
+    or a 400 if there are invalid parameters.
 
     Args:
       client_id: The client ID.
+      metric_id: The metric ID. Must be an empty string.
     """
+    if metric_id:
+      self.error(400)  # Bad request.
+      return
+
     id = self.request.get('id', None)
     desc = self.request.get('description', None)
     units = self.request.get('units', None)
@@ -82,8 +87,8 @@ class MetricHandler(webapp.RequestHandler):
       self.error(404)  # Not found.
       return
 
-    # Creates a new metric or updates the parent, description and units for an
-    # existing metric.
+    # Creates a new metric or updates the description and units for an existing
+    # metric.
     metric = metric_db.Metric(key_name=id, parent=client, description=desc,
                               units=units)
     metric.put()
