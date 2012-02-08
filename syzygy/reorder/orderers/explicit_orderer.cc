185194adcb2b73784f9666dfd6545776b215ef81
Chris Hamilton <chrisha@chromium.org>
2012-02-08 18:50:06 +0000

    Create ExplicitOrderer.
    This CL creates an orderer that applies the order information specified by a Reorderer::Order object. All of this will eventually disappear in a post-metadata world, but this acts as a bridge between the old code (reorder_lib) and the new code (transforms and orderers) until block-graph metadata is available.
    
    BUG=
    TEST=
    
    Review URL: https://codereview.appspot.com/5645049
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@633 15e8cca8-e42c-11de-a347-f34a4f72eb7d

---
+++ syzygy/reorder/orderers/explicit_orderer.cc
@@ -0,0 +1,105 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "syzygy/reorder/orderers/explicit_orderer.h"
+
+#include <algorithm>
+#include <vector>
+
+#include "base/stringprintf.h"
+
+namespace reorder {
+namespace orderers {
+
+namespace {
+
+using core::RelativeAddress;
+
+typedef std::vector<BlockGraph::Block*> Blocks;
+
+void GetSortedBlocks(BlockGraph* block_graph, Blocks* blocks) {
+  DCHECK(block_graph != NULL);
+  DCHECK(blocks != NULL);
+
+  blocks->clear();
+  blocks->reserve(block_graph->blocks().size());
+
+  BlockGraph::BlockMap::iterator block_it =
+      block_graph->blocks_mutable().begin();
+  for (; block_it != block_graph->blocks_mutable().end(); ++block_it) {
+    BlockGraph::Block* block = &block_it->second;
+    blocks->push_back(block);
+  }
+
+  // Sort by block address.
+  std::sort(blocks->begin(), blocks->end());
+}
+
+}  // namespace
+
+const char ExplicitOrderer::kOrdererName[] = "ExplicitOrderer";
+
+bool ExplicitOrderer::Apply(OrderedBlockGraph* ordered_block_graph,
+                         BlockGraph::Block* /* header_block */) {
+  DCHECK(ordered_block_graph != NULL);
+  DCHECK(order_ != NULL);
+
+  BlockGraph* bg = ordered_block_graph->block_graph();
+
+  typedef Reorderer::Order::BlockListMap BlockListMap;
+  typedef Reorderer::Order::BlockList BlockList;
+
+  Blocks sorted_blocks;
+  GetSortedBlocks(bg, &sorted_blocks);
+
+  BlockListMap::const_iterator section_it = order_->section_block_lists.begin();
+  for (; section_it != order_->section_block_lists.end(); ++section_it) {
+    // Find the section in the original block-graph with the same ID.
+    BlockGraph::Section* section = bg->GetSectionById(section_it->first);
+    if (section == NULL) {
+      LOG(ERROR) << "No section found with ID " << section_it->first << ".";
+      return false;
+    }
+
+    LOG(INFO) << "Applying order to section " << section_it->first
+              << "(" << section->name() << ").";
+
+    // We walk through these in reverse order so that we can use PlaceAtHead.
+    for (size_t i = section_it->second.size(); i > 0; --i) {
+      // Look for the block with the matching address in memory. We do this
+      // just in case the BlockGraph has evolved since the order object was
+      // built.
+      const BlockGraph::Block* block = section_it->second[i - 1];
+      Blocks::const_iterator block_it =
+          std::lower_bound(sorted_blocks.begin(),
+                           sorted_blocks.end(),
+                           block);
+
+      // Not found?
+      if (block_it == sorted_blocks.end() || *block_it != block) {
+        LOG(ERROR) << "Block specified in order does not exist in BlockGraph.";
+        return false;
+      }
+
+      // At this point we have a single unique block that we've found, so
+      // place it at the beginning of the section.
+      ordered_block_graph->PlaceAtHead(section, *block_it);
+    }
+  }
+
+  return true;
+}
+
+}  // namespace orderers
+}  // namespace reorder
---
+++ syzygy/reorder/orderers/explicit_orderer.h
@@ -0,0 +1,59 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Declares an ordering that orders blocks as explicitly specified in an
+// Reorderer::Order object. Blocks are uniquely specified by their addresses
+// in the most recent ImageLayout in which they took part.
+
+#ifndef SYZYGY_REORDER_ORDERERS_EXPLICIT_ORDERER_H_
+#define SYZYGY_REORDER_ORDERERS_EXPLICIT_ORDERER_H_
+
+#include "syzygy/block_graph/orderers/named_orderer.h"
+#include "syzygy/reorder/reorderer.h"
+
+namespace reorder {
+namespace orderers {
+
+class ExplicitOrderer
+    : public block_graph::orderers::NamedOrdererImpl<ExplicitOrderer> {
+ public:
+  typedef block_graph::BlockGraph BlockGraph;
+  typedef block_graph::OrderedBlockGraph OrderedBlockGraph;
+
+  // Constructor.
+  // @param order a pointer to the order to be applied. The order object must
+  //     outlive this orderer.
+  explicit ExplicitOrderer(const Reorderer::Order* order) : order_(order) {
+    DCHECK(order != NULL);
+  }
+
+  // Applies this orderer to the provided block graph.
+  //
+  // @param ordered_block_graph the block graph to order.
+  // @returns true on success, false otherwise.
+  virtual bool Apply(OrderedBlockGraph* ordered_block_graph,
+                     BlockGraph::Block* header_block_unused) OVERRIDE;
+
+  static const char kOrdererName[];
+
+ private:
+  const Reorderer::Order* order_;
+
+  DISALLOW_COPY_AND_ASSIGN(ExplicitOrderer);
+};
+
+}  // namespace orderers
+}  // namespace reorder
+
+#endif  // SYZYGY_REORDER_ORDERERS_EXPLICIT_ORDERER_H_
