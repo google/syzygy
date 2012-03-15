cd0eebcda312bda9940ee2b939d22dac663b1fae
Chris Hamilton <chrisha@chromium.org>
2012-03-15 20:57:37 +0000

    Create PdbFile, a lightweight in-memory representation of a PDB.
    This CL creates PdbFile. A follow-up CL will rework PdbReader so that it populates a PdbFile, and rework PdbWriter to accept PdbFiles as input.
    
    BUG=
    TEST=
    
    Review URL: https://codereview.appspot.com/5824052
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@706 15e8cca8-e42c-11de-a347-f34a4f72eb7d

---
+++ syzygy/pdb/pdb_file.cc
@@ -0,0 +1,48 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "syzygy/pdb/pdb_file.h"
+
+namespace pdb {
+
+PdbFile::PdbFile() {
+}
+
+PdbFile::~PdbFile() {
+  // Free up the existing streams.
+  for (size_t i = 0; i < streams_.size(); ++i) {
+    if (streams_[i] != NULL)
+      delete streams_[i];
+  }
+}
+
+PdbStream* PdbFile::GetStream(uint32 index) {
+  DCHECK_LT(index, streams_.size());
+  return streams_[index];
+}
+
+size_t PdbFile::AppendStream(PdbStream* pdb_stream) {
+  size_t index = streams_.size();
+  streams_.push_back(pdb_stream);
+  return index;
+}
+
+void PdbFile::ReplaceStream(uint32 index, PdbStream* pdb_stream) {
+  DCHECK_LT(index, streams_.size());
+  if (streams_[index] != NULL)
+    delete streams_[index];
+  streams_[index] = pdb_stream;
+}
+
+}  // namespace pdb
---
+++ syzygy/pdb/pdb_file.h
@@ -0,0 +1,76 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Declares a PDB file, which is an in-memory representation of a PDB file.
+// A PDB file consists of a collection of numbered PDB streams. The streams
+// themselves obey certain formats and conventions but these are not enforced
+// by this naive representation.
+
+#ifndef SYZYGY_PDB_PDB_FILE_H_
+#define SYZYGY_PDB_PDB_FILE_H_
+
+#include <vector>
+
+#include "syzygy/pdb/pdb_stream.h"
+
+namespace pdb {
+
+// A simple representation of a PDB file as a collection of numbered streams.
+// This object owns all of the streams referred to by it and maintains
+// responsibility for cleaning them up on destruction.
+class PdbFile {
+ public:
+  PdbFile();
+  ~PdbFile();
+
+  // Accesses the nth stream.
+  // @param index the index of the nth stream.
+  // @returns a pointer to the stream, NULL if it does not exist.
+  PdbStream* GetStream(uint32 index);
+
+  // Adds a new stream to this PDB file, returning the index of the newly
+  // generated stream. The stream must be heap allocated and ownership is
+  // passed to this object.
+  // @param pdb_stream a pointer to a heap allocated stream object. Ownership of
+  //     the stream is passed to this object. This may be NULL, indicating that
+  //     the nth stream exists but is empty.
+  // @returns the index of the added stream.
+  size_t AppendStream(PdbStream* pdb_stream);
+
+  // Sets the nth stream. The stream object must be heap allocated and
+  // ownership is implicitly transferred to the PdbFile object. Overwrites an
+  // existing stream if there is one, and destroys it as well. It is up to the
+  // caller to ensure there are no outstanding references to the existing
+  // stream.
+  // @param index the index of the stream. This must be >= 0, and must be
+  //     a stream index that already exists.
+  // @param pdb_stream a pointer to the heap allocated stream to be placed at
+  //     the given position. Ownership is transferred to this object. This may
+  //     be NULL, which is equivalent to erasing the given stream.
+  void ReplaceStream(uint32 index, PdbStream* pdb_stream);
+
+  // Returns the number of streams in the PDB file. There are streams with
+  // IDs 0 through StreamCount() - 1.
+  // @returns the number of streams in the PDB file.
+  size_t StreamCount() const { return streams_.size(); }
+
+ private:
+  // The streams are implicitly numbered simply by their position in this
+  // vector.
+  std::vector<PdbStream*> streams_;
+};
+
+}  // namespace pdb
+
+#endif  // SYZYGY_PDB_PDB_FILE_H_
