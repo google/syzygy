8f606bcee8aa5458fb9c3b6255d255a1b048ac5b
ericdingle@chromium.org <ericdingle@chromium.org@15e8cca8-e42c-11de-a347-f34a4f72eb7d>
2012-03-20 14:24:26 +0000

    Client handler unit test.
    BUG=
    TEST=
    
    Review URL: https://codereview.appspot.com/5841049
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk/syzygy@713 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- dashboard/test/handler/product_test.py
+++ dashboard/test/handler/product_test.py
@@ -15,21 +15,15 @@
 
 import httplib
 import json
-import StringIO
-import unittest
-from google.appengine.ext import testbed
-from google.appengine.ext import webapp
 from handler import product
 from model import client as client_db
 from model import product as product_db
+from test.handler import handler_test
 
-class ProductTest(unittest.TestCase):
+class ProductTest(handler_test.TestCase):
 
   def setUp(self):
-    # Set up the test bed.
-    self._testbed = testbed.Testbed()
-    self._testbed.activate()
-    self._testbed.init_datastore_v3_stub()
+    super(ProductTest, self).setUp()
 
     # Insert values into the db.
     p1 = product_db.Product(key_name='p1')
@@ -38,77 +32,76 @@ class ProductTest(unittest.TestCase):
 
     product_db.Product(key_name='p2').put()
 
-  def tearDown(self):
-    self._testbed.deactivate()
-
-  def _Init(self, input):
-    """Create fake request, response and handler."""
-    self._request = webapp.Request({
-        'wsgi.input': StringIO.StringIO(input),
-        'CONTENT_LENGTH': len(input),
-        'REQUEST_METHOD': 'POST'})
-    self._response = webapp.Response()
-
+  def _InitHandler(self, input):
     self._handler = product.ProductHandler()
-    self._handler.initialize(self._request, self._response)
+    super(ProductTest, self)._InitHandler(self._handler, input)
 
   def testGetAll(self):
-    self._Init('')
+    self._InitHandler('')
     self._handler.get('')
     self.assertEqual(httplib.OK, self._response.status)
-    result = json.loads(self._response.out.getvalue())
-    products = result.get('products')
-    self.assertEqual(2, len(products))
-    self.assertEqual('p1', products[0].get('product_id'))
-    self.assertEqual('p2', products[1].get('product_id'))
 
-  def testGetByID(self):
-    self._Init('')
+    result = json.loads(self._response.out.getvalue())
+    self.assertEqual(
+        {'products': [
+            {'product_id': 'p1'},
+            {'product_id': 'p2'}
+        ]},
+        result)
+
+  def testGetById(self):
+    self._InitHandler('')
     self._handler.get('p1')
     self.assertEqual(httplib.OK, self._response.status)
+
     result = json.loads(self._response.out.getvalue())
-    self.assertEqual('p1', result.get('product_id'))
-    self.assertEqual(['c1'], result.get('client_ids'))
+    self.assertEqual(
+        {'product_id': 'p1',
+         'client_ids': ['c1']},
+        result)
 
   def testGetArgValidation(self):
-    self._Init('')
+    self._InitHandler('')
     self._handler.get('blah')
     self.assertEqual(httplib.NOT_FOUND, self._response.status)
 
   def testPost(self):
-    self._Init('product_id=p3')
+    self._InitHandler('product_id=p3')
     self._handler.post('')
     self.assertEqual(httplib.CREATED, self._response.status)
 
+    self.assertTrue(product_db.Product.get_by_key_name('p3') is not None)
+
   def testPostArgValidation(self):
-    self._Init('')
-    self._handler.post('p1')
+    self._InitHandler('product_id=p3')
+    self._handler.post('p3')
     self.assertEqual(httplib.BAD_REQUEST, self._response.status)
 
-    self._Init('')
+    self._InitHandler('')
     self._handler.post('')
     self.assertEqual(httplib.BAD_REQUEST, self._response.status)
 
-    self._Init('product_id=p1')
+    self._InitHandler('product_id=p1')
     self._handler.post('')
     self.assertEqual(httplib.BAD_REQUEST, self._response.status)
 
   def testPut(self):
-    self._Init('')
+    self._InitHandler('')
     self._handler.put()
     self.assertEqual(httplib.METHOD_NOT_ALLOWED, self._response.status)
 
   def testDelete(self):
-    self._Init('')
+    self._InitHandler('')
     self._handler.delete('p1')
     self.assertEqual(httplib.OK, self._response.status)
-    self.assertEqual(None, product_db.Product.get_by_key_name('p1'))
+
+    self.assertTrue(product_db.Product.get_by_key_name('p1') is None)
 
   def testDeleteArgValidation(self):
-    self._Init('')
+    self._InitHandler('')
     self._handler.delete('')
     self.assertEqual(httplib.BAD_REQUEST, self._response.status)
 
-    self._Init('')
+    self._InitHandler('')
     self._handler.delete('blah')
     self.assertEqual(httplib.NOT_FOUND, self._response.status)
