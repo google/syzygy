d8b2b78ee346afa775602279cf3fded3ea37aec8
Sigurdur Asgeirsson <siggi@chromium.org>
2012-03-26 15:00:02 +0000

    Initial implementation and minimal testing for grinder, the profile data processor that outputs KCacheGrind-compatible output files.
    Review URL: https://codereview.appspot.com/5841063
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@734 15e8cca8-e42c-11de-a347-f34a4f72eb7d

---
+++ syzygy/grinder/grinder.h
@@ -0,0 +1,280 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef SYZYGY_GRINDER_GRINDER_H_
+#define SYZYGY_GRINDER_GRINDER_H_
+
+#include <dia2.h>
+#include <iostream>
+#include <map>
+
+#include "base/file_path.h"
+#include "base/win/scoped_comptr.h"
+#include "syzygy/common/application.h"
+#include "syzygy/trace/parse/parser.h"
+
+namespace grinder {
+
+// Fwd.
+struct InvocationEdge;
+struct InvocationNode;
+struct Metrics;
+struct ModuleRVA;
+typedef uint32 RVA;
+
+// A worker class to sink profile trace events and output the aggregate data in
+// KCacheGrind-compatible format.
+//
+// The profiler instrumentation captures the wall-clock time from entry to exit
+// for each pair of caller/function for each invocation. This is termed
+// "inclusive" time, as it includes the time spent in other functions called.
+//
+// The KCacheGrind file format also requires listing "exclusive" time for each
+// function, where exclusive time is the amount of time spent executing the
+// function itself, e.g. exclusive of the time spent calling other functions.
+//
+// The profile data is captured in a trace log. The trace log is a run of
+// records where each record in the log is associated with a particular thread
+// (and process), and contains a set of invocation records.
+// Each invocation record contains inclusive wall-clock time (and potentially
+// other inclusive metrics) for one or more invocations from a particular
+// caller address, to a particular function.
+// Note that the same caller/function pair may occur multiple times in a trace
+// log, even for the same thread, as the profile instrumentation spills a trace
+// record to the log when the number of caller/function pairs it's seen exeeds
+// the size of the thread-local buffer used to aggregate the data.
+//
+// This class aggregates the data from a trace log, and builds a graph of
+// function nodes and call edges. For each call edge, it aggregates the data
+// from one or more log records, by summing up the call counts and inclusive
+// metrics. For each function node, it also computes the exlusive cost, by
+// summing up the cost of the incoming edges, and subtracting the cost of the
+// outgoing edges.
+//
+// For information on the KCacheGrind file format, see:
+// http://kcachegrind.sourceforge.net/cgi-bin/show.cgi/KcacheGrindCalltreeFormat
+class Grinder : public trace::parser::ParseEventHandler {
+ public:
+  typedef trace::parser::Parser Parser;
+
+  Grinder();
+  ~Grinder();
+
+  // @name Accessors.
+  // @{
+  const Parser* parser() const { return parser_; }
+  void set_parser(Parser* parser) { parser_ = parser; }
+  // @}
+
+  // Invoke after processing all traces.
+  // This functions adds all caller edges to each function node's linked list of
+  // callers. In so doing, it also computes each function node's inclusive cost.
+  // @returns true on success, false on failure.
+  bool ResolveCallers();
+
+  // Invoke after ResolveCallers.
+  // Outputs the aggregated data to @p file.
+  // @returns true on success, false on failure.
+  bool OutputData(FILE* file);
+
+  // @name ParseEventHandler implementation.
+  // @{
+  void OnProcessStarted(base::Time time,
+                        DWORD process_id,
+                        const TraceSystemInfo* data) OVERRIDE;
+  void OnProcessEnded(base::Time time, DWORD process_id) OVERRIDE;
+  void OnFunctionEntry(base::Time time,
+                       DWORD process_id,
+                       DWORD thread_id,
+                       const TraceEnterExitEventData* data) OVERRIDE;
+  void OnFunctionExit(base::Time time,
+                      DWORD process_id,
+                      DWORD thread_id,
+                      const TraceEnterExitEventData* data) OVERRIDE;
+  void OnBatchFunctionEntry(base::Time time,
+                            DWORD process_id,
+                            DWORD thread_id,
+                            const TraceBatchEnterData* data) OVERRIDE;
+  void OnProcessAttach(base::Time time,
+                       DWORD process_id,
+                       DWORD thread_id,
+                       const TraceModuleData* data) OVERRIDE;
+  void OnProcessDetach(base::Time time,
+                       DWORD process_id,
+                       DWORD thread_id,
+                       const TraceModuleData* data) OVERRIDE;
+  void OnThreadAttach(base::Time time,
+                      DWORD process_id,
+                      DWORD thread_id,
+                      const TraceModuleData* data) OVERRIDE;
+  void OnThreadDetach(base::Time time,
+                      DWORD process_id,
+                      DWORD thread_id,
+                      const TraceModuleData* data) OVERRIDE;
+  void OnInvocationBatch(base::Time time,
+                         DWORD process_id,
+                         DWORD thread_id,
+                         size_t num_invocations,
+                         const TraceBatchInvocationInfo* data) OVERRIDE;
+  // @}
+ private:
+  typedef sym_util::ModuleInformation ModuleInformation;
+
+  typedef std::set<ModuleInformation,
+      bool (*)(const ModuleInformation& a, const ModuleInformation& b)>
+          ModuleInformationSet;
+  typedef std::set<InvocationNode,
+      bool (*)(const InvocationNode& a,
+               const InvocationNode& b)> InvocationNodeSet;
+  typedef std::set<InvocationEdge,
+      bool (*)(const InvocationEdge& a,
+               const InvocationEdge& b)> InvocationEdgeSet;
+
+  typedef base::win::ScopedComPtr<IDiaSession> SessionPtr;
+  typedef std::map<const ModuleInformation*, SessionPtr> ModuleSessionMap;
+
+  bool GetSessionForModule(const ModuleInformation* module,
+                           IDiaSession** session_out);
+
+  bool GetInfoForCallerRVA(const ModuleRVA& caller,
+                           RVA* function_rva,
+                           size_t* line);
+
+  bool GetInfoForFunctionRVA(const ModuleRVA& function,
+                             std::wstring* function_name,
+                             std::wstring* file_name,
+                             size_t* line);
+
+  // Converts an absolute address to an RVA.
+  void ConvertToModuleRVA(uint32 process_id,
+                          trace::parser::AbsoluteAddress64 addr,
+                          ModuleRVA* rva);
+
+  // Aggregates a single invocation info and/or creates a new node and edge.
+  void AggregateEntry(const ModuleRVA& function_rva,
+                      const ModuleRVA& caller_rva,
+                      const InvocationInfo& info);
+
+  // Stores the modules we encounter.
+  ModuleInformationSet modules_;
+
+  // Stores the invocation nodes, aka the functions.
+  InvocationNodeSet nodes_;
+
+  // Stores the invocation edges.
+  InvocationEdgeSet edges_;
+
+  // Stores the DIA session objects we have going for each module.
+  ModuleSessionMap module_sessions_;
+
+  Parser* parser_;
+};
+
+// RVA in a module. The module should be a canonical pointer
+// to the module information to make this comparable against
+// other RVAs in the same module.
+struct ModuleRVA {
+  ModuleRVA() : module(NULL), rva(0) {
+  }
+
+  bool operator < (const ModuleRVA& o) const {
+    if (module > o.module)
+      return false;
+    if (module < o.module)
+      return true;
+    return rva < o.rva;
+  }
+  bool operator > (const ModuleRVA& o) const {
+    return o < *this;
+  }
+  bool operator == (const ModuleRVA& o) const {
+    return !(o < *this || *this < o);
+  }
+  bool operator != (const ModuleRVA& o) const {
+    return !(*this == o);
+  }
+
+  sym_util::ModuleInformation* module;
+  RVA rva;
+};
+
+// The metrics we capture per function and per caller.
+struct Metrics {
+  Metrics() : num_calls(0), cycles_min(0), cycles_max(0), cycles_sum(0) {
+  }
+
+  uint64 num_calls;
+  uint64 cycles_min;
+  uint64 cycles_max;
+  uint64 cycles_sum;
+};
+
+// An invocation node represents a function.
+struct InvocationNode {
+  InvocationNode() : first_call(NULL) {
+  }
+
+  // RVA for the function this instance represents.
+  ModuleRVA function;
+
+  // The metrics we've aggregated for this function.
+  Metrics metrics;
+
+  // Linked list of all the edges where the caller resolves to us.
+  InvocationEdge* first_call;
+};
+
+// An invocation edge represents a caller->function pair.
+struct InvocationEdge {
+  InvocationEdge() : caller_function(NULL), line(0), next_call(NULL) {
+  }
+
+  // The function/caller pair we denote.
+  ModuleRVA function;
+  ModuleRVA caller;
+
+  // Line number of the caller.
+  size_t line;
+  Metrics metrics;
+
+  // The calling function - resolved from caller.
+  InvocationNode* caller_function;
+  // Chains to the next edge resolving to the
+  // same calling function.
+  InvocationEdge* next_call;
+};
+
+// The application class that takes care of running Grinder over a set of
+// profiler trace files.
+class GrinderApp : public common::AppImplBase {
+ public:
+  // @name Implementation of the AppImplbase interface.
+  // @{
+  bool ParseCommandLine(const CommandLine* command_line);
+  int Run();
+  // @}
+
+  // @name Utility functions
+  // @{
+  void PrintUsage(const FilePath& program,
+                  const base::StringPiece& message);
+  // @}
+
+ protected:
+  std::vector<FilePath> trace_files_;
+  FilePath output_file_;
+};
+
+}  // namespace grinder
+
+#endif  // SYZYGY_GRINDER_GRINDER_H_
---
+++ syzygy/grinder/grinder_unittest.cc
@@ -0,0 +1,145 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "syzygy/grinder/grinder.h"
+
+#include "gtest/gtest.h"
+#include "syzygy/common/application.h"
+#include "syzygy/core/unittest_util.h"
+#include "syzygy/pe/unittest_util.h"
+
+namespace grinder {
+
+namespace {
+
+class TestGrinderApp : public GrinderApp {
+ public:
+  // Expose for testing.
+  using GrinderApp::trace_files_;
+  using GrinderApp::output_file_;
+};
+
+class GrinderAppTest : public testing::PELibUnitTest {
+ public:
+  typedef testing::PELibUnitTest Super;
+  typedef common::Application<TestGrinderApp> TestApplication;
+
+  GrinderAppTest()
+      : cmd_line_(FilePath(L"grinder.exe")),
+        impl_(app_.implementation()) {
+  }
+
+  void SetUp() {
+    Super::SetUp();
+
+    // Setup the IO streams.
+    CreateTemporaryDir(&temp_dir_);
+    stdin_path_ = temp_dir_.Append(L"NUL");
+    stdout_path_ = temp_dir_.Append(L"stdout.txt");
+    stderr_path_ = temp_dir_.Append(L"stderr.txt");
+    InitStreams(stdin_path_, stdout_path_, stderr_path_);
+
+    // Point the application at the test's command-line and IO streams.
+    app_.set_command_line(&cmd_line_);
+    app_.set_in(in());
+    app_.set_out(out());
+    app_.set_err(err());
+  }
+
+ protected:
+  // The command line to be given to the application under test.
+  CommandLine cmd_line_;
+
+  // The application object under test.
+  TestApplication app_;
+
+  // A reference to the underlying application implementation for convenience.
+  TestGrinderApp& impl_;
+
+  // A temporary folder where all IO will be stored.
+  FilePath temp_dir_;
+
+  // @name File paths used for the standard IO streams.
+  // @{
+  FilePath stdin_path_;
+  FilePath stdout_path_;
+  FilePath stderr_path_;
+  // @}
+};
+
+}  // namespace
+
+TEST_F(GrinderAppTest, ParseCommandLineFailsWithNoFiles) {
+  ASSERT_FALSE(impl_.ParseCommandLine(&cmd_line_));
+}
+
+TEST_F(GrinderAppTest, ParseCommandLineTraceFiles) {
+  std::vector<FilePath> temp_files;
+  for (size_t i = 0; i < 10; ++i) {
+    FilePath temp_file;
+    ASSERT_TRUE(file_util::CreateTemporaryFileInDir(temp_dir_, &temp_file));
+    cmd_line_.AppendArgPath(temp_file);
+    temp_files.push_back(temp_file);
+  }
+
+  ASSERT_TRUE(impl_.ParseCommandLine(&cmd_line_));
+  ASSERT_EQ(temp_files, impl_.trace_files_);
+}
+
+TEST_F(GrinderAppTest, ParseCommandLineOutputFile) {
+  ASSERT_TRUE(impl_.output_file_.empty());
+  cmd_line_.AppendSwitchPath("output-file", FilePath(L"output.txt"));
+  cmd_line_.AppendArg("foo.bin");
+
+  ASSERT_TRUE(impl_.ParseCommandLine(&cmd_line_));
+  ASSERT_EQ(L"output.txt", impl_.output_file_.value());
+}
+
+TEST_F(GrinderAppTest, EndToEnd) {
+  cmd_line_.AppendArgPath(
+      testing::GetExeTestDataRelativePath(L"profile_traces/trace-1.bin"));
+
+  FilePath output_file;
+  ASSERT_TRUE(file_util::CreateTemporaryFileInDir(temp_dir_, &output_file));
+  ASSERT_TRUE(file_util::Delete(output_file, false));
+  cmd_line_.AppendSwitchPath("output-file", output_file);
+
+  ASSERT_TRUE(!file_util::PathExists(output_file));
+
+  // TODO(siggi): Improve the module info->PDB lookup to properly resolve
+  //     the device paths from the call trace to on-disk paths. In the
+  //     meantime, we change the working directory to where the instrumented
+  //     module resides.
+  FilePath cwd;
+  ASSERT_TRUE(file_util::GetCurrentDirectory(&cwd));
+  ASSERT_TRUE(file_util::SetCurrentDirectory(
+      testing::GetExeTestDataRelativePath(L".")));
+
+  EXPECT_EQ(0, app_.Run());
+
+  // Verify that the output file was created.
+  EXPECT_TRUE(file_util::PathExists(output_file));
+
+  ASSERT_TRUE(file_util::SetCurrentDirectory(cwd));
+}
+
+} //  namespace grinder
+
+int main(int argc, char** argv) {
+  base::AtExitManager at_exit;
+  CommandLine::Init(argc, argv);
+
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
