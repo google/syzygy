931bf9011892b69d07a3320bdb72f06ae6a02258
Sigurdur Asgeirsson <siggi@chromium.org>
2012-05-01 21:30:50 +0000

    Fix non-compilant names in assembler.
    Review URL: https://codereview.appspot.com/6133066
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@795 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- syzygy/core/assembler_unittest.cc
+++ syzygy/core/assembler_unittest.cc
@@ -103,7 +103,7 @@ TEST_F(AssemblerTest, OperandImpl) {
     OperandImpl op1(edi);
     EXPECT_EQ(kRegisterEdi, op1.base());
     EXPECT_EQ(kRegisterNone, op1.index());
-    EXPECT_EQ(times_1, op1.scale());
+    EXPECT_EQ(kTimes1, op1.scale());
     EXPECT_EQ(0, op1.displacement().value());
     EXPECT_EQ(NULL, op1.displacement().reference());
     EXPECT_EQ(kSizeNone, op1.displacement().size());
@@ -114,7 +114,7 @@ TEST_F(AssemblerTest, OperandImpl) {
     OperandImpl op2(ecx, DisplacementImpl(0xCAFEBABE, kSize32Bit, &ref2));
     EXPECT_EQ(kRegisterEcx, op2.base());
     EXPECT_EQ(kRegisterNone, op2.index());
-    EXPECT_EQ(times_1, op2.scale());
+    EXPECT_EQ(kTimes1, op2.scale());
     EXPECT_EQ(0xCAFEBABE, op2.displacement().value());
     EXPECT_EQ(&ref2, op2.displacement().reference());
     EXPECT_EQ(kSize32Bit, op2.displacement().size());
@@ -125,7 +125,7 @@ TEST_F(AssemblerTest, OperandImpl) {
     OperandImpl op3(DisplacementImpl(0xCAFEBABE, kSize32Bit, &ref3));
     EXPECT_EQ(kRegisterNone, op3.base());
     EXPECT_EQ(kRegisterNone, op3.index());
-    EXPECT_EQ(times_1, op3.scale());
+    EXPECT_EQ(kTimes1, op3.scale());
     EXPECT_EQ(0xCAFEBABE, op3.displacement().value());
     EXPECT_EQ(&ref3, op3.displacement().reference());
     EXPECT_EQ(kSize32Bit, op3.displacement().size());
@@ -134,10 +134,10 @@ TEST_F(AssemblerTest, OperandImpl) {
   {
     int ref4 = 0;
     OperandImpl
-        op4(ebp, ecx, times_2, DisplacementImpl(0xCA, kSize8Bit, &ref4));
+        op4(ebp, ecx, kTimes2, DisplacementImpl(0xCA, kSize8Bit, &ref4));
     EXPECT_EQ(kRegisterEbp, op4.base());
     EXPECT_EQ(kRegisterEcx, op4.index());
-    EXPECT_EQ(times_2, op4.scale());
+    EXPECT_EQ(kTimes2, op4.scale());
     EXPECT_EQ(0xCA, op4.displacement().value());
     EXPECT_EQ(&ref4, op4.displacement().reference());
     EXPECT_EQ(kSize8Bit, op4.displacement().size());
@@ -327,91 +327,91 @@ TEST_F(AssemblerTest, MovRegisterDisplacementScaleIndirect) {
   DisplacementImpl cafebabe(0xCAFEBABE, kSize32Bit, NULL);
 
   // Source mode, base register.
-  asm_.mov(edx, OperandImpl(ecx, eax, times_4, cafebabe));
+  asm_.mov(edx, OperandImpl(ecx, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x94, 0x81, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(ecx, eax, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(ecx, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x81, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(edx, eax, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(edx, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x82, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(ebx, eax, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(ebx, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x83, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(esp, eax, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(esp, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x84, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(ebp, eax, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(ebp, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x85, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(esi, eax, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(esi, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x86, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(edi, eax, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(edi, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x87, 0xBE, 0xBA, 0xFE, 0xCA);
 
   // Source mode, index register.
-  asm_.mov(ebx, OperandImpl(ecx, eax, times_4, cafebabe));
+  asm_.mov(ebx, OperandImpl(ecx, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x9C, 0x81, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(eax, ecx, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(eax, ecx, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x88, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(eax, edx, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(eax, edx, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x90, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(eax, ebx, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(eax, ebx, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0x98, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(eax, ebp, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(eax, ebp, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0xA8, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(eax, esi, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(eax, esi, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0xB0, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(eax, OperandImpl(eax, edi, times_4, cafebabe));
+  asm_.mov(eax, OperandImpl(eax, edi, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x84, 0xB8, 0xBE, 0xBA, 0xFE, 0xCA);
 
   // Source mode, Scale.
-  asm_.mov(ebx, OperandImpl(ecx, eax, times_1, cafebabe));
+  asm_.mov(ebx, OperandImpl(ecx, eax, kTimes1, cafebabe));
   EXPECT_BYTES(0x8B, 0x9C, 0x01, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(ebx, OperandImpl(ecx, eax, times_2, cafebabe));
+  asm_.mov(ebx, OperandImpl(ecx, eax, kTimes2, cafebabe));
   EXPECT_BYTES(0x8B, 0x9C, 0x41, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(ebx, OperandImpl(ecx, eax, times_4, cafebabe));
+  asm_.mov(ebx, OperandImpl(ecx, eax, kTimes4, cafebabe));
   EXPECT_BYTES(0x8B, 0x9C, 0x81, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(ebx, OperandImpl(ecx, eax, times_8, cafebabe));
+  asm_.mov(ebx, OperandImpl(ecx, eax, kTimes8, cafebabe));
   EXPECT_BYTES(0x8B, 0x9C, 0xC1, 0xBE, 0xBA, 0xFE, 0xCA);
 
   // Destination mode, base register.
-  asm_.mov(OperandImpl(eax, eax, times_4, cafebabe), ecx);
+  asm_.mov(OperandImpl(eax, eax, kTimes4, cafebabe), ecx);
   EXPECT_BYTES(0x89, 0x8C, 0x80, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(ecx, eax, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(ecx, eax, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x81, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(edx, eax, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(edx, eax, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x82, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(ebx, eax, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(ebx, eax, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x83, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(esp, eax, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(esp, eax, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x84, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(ebp, eax, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(ebp, eax, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x85, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(esi, eax, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(esi, eax, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x86, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(edi, eax, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(edi, eax, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x87, 0xBE, 0xBA, 0xFE, 0xCA);
 
   // Destination mode, index register.
-  asm_.mov(OperandImpl(ecx, eax, times_4, cafebabe), ebx);
+  asm_.mov(OperandImpl(ecx, eax, kTimes4, cafebabe), ebx);
   EXPECT_BYTES(0x89, 0x9C, 0x81, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(eax, ecx, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(eax, ecx, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x88, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(eax, edx, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(eax, edx, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x90, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(eax, ebx, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(eax, ebx, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0x98, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(eax, ebp, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(eax, ebp, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0xA8, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(eax, esi, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(eax, esi, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0xB0, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(eax, edi, times_4, cafebabe), eax);
+  asm_.mov(OperandImpl(eax, edi, kTimes4, cafebabe), eax);
   EXPECT_BYTES(0x89, 0x84, 0xB8, 0xBE, 0xBA, 0xFE, 0xCA);
 
   // Destination mode, Scale.
-  asm_.mov(OperandImpl(ecx, eax, times_1, cafebabe), ebx);
+  asm_.mov(OperandImpl(ecx, eax, kTimes1, cafebabe), ebx);
   EXPECT_BYTES(0x89, 0x9C, 0x01, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(ecx, eax, times_2, cafebabe), ebx);
+  asm_.mov(OperandImpl(ecx, eax, kTimes2, cafebabe), ebx);
   EXPECT_BYTES(0x89, 0x9C, 0x41, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(ecx, eax, times_4, cafebabe), ebx);
+  asm_.mov(OperandImpl(ecx, eax, kTimes4, cafebabe), ebx);
   EXPECT_BYTES(0x89, 0x9C, 0x81, 0xBE, 0xBA, 0xFE, 0xCA);
-  asm_.mov(OperandImpl(ecx, eax, times_8, cafebabe), ebx);
+  asm_.mov(OperandImpl(ecx, eax, kTimes8, cafebabe), ebx);
   EXPECT_BYTES(0x89, 0x9C, 0xC1, 0xBE, 0xBA, 0xFE, 0xCA);
 }
 
