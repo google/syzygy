ec82637a331cdda79a4ddf484b85cac4b9ce6223
rogerm@chromium.org <rogerm@chromium.org@15e8cca8-e42c-11de-a347-f34a4f72eb7d>
2012-05-23 15:06:29 +0000

    Switch dos stub processor spec to 386.
    286 support is removed from the MASM that ships with VS2010.
    
    BUG=
    TEST=
    
    Review URL: https://codereview.appspot.com/6223063
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk/syzygy@834 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- pe/dos_stub.asm
+++ pe/dos_stub.asm
@@ -1,4 +1,4 @@
-; Copyright 2011 Google Inc.
+; Copyright 2012 Google Inc.
 ;
 ; Licensed under the Apache License, Version 2.0 (the "License");
 ; you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 ; limitations under the License.
 ;
 
-.286
+.386
 .MODEL TINY, C
 
 .CODE
