babc43d60bcd6f5070092fc08e3bbf5faf2921d8
Chris Hamilton <chrisha@chromium.org>
2012-05-25 13:57:53 +0000

    Pylint cleanup part 3.
    BUG=
    TEST=
    
    Review URL: https://codereview.appspot.com/6247044
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@846 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- syzygy/scripts/graph/setup.py
+++ syzygy/scripts/graph/setup.py
@@ -1,5 +1,5 @@
 #!python
-# Copyright 2011 Google Inc.
+# Copyright 2012 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from setuptools import setup
+from setuptools import setup  # pylint: disable=F0401
 
 setup(
     name='Graph-Pagefaults',
