dc326b20eabc049891175cadd68661436fcd3030
rogerm@chromium.org <rogerm@chromium.org@15e8cca8-e42c-11de-a347-f34a4f72eb7d>
2012-07-06 14:05:29 +0000

    Refactor Instruction helpers.
    This CL addresses some TODOs to moved some helper function to be members of the block_graph::Instruction class and adjusts the basic block control flow validation to use the new helpers.
    
    It also tightens the scenarios in which a empty successor set is considered valid post basic-block decomposition.
    
    BUG=
    TEST=
    
    Review URL: https://codereview.appspot.com/6356061
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk/syzygy@969 15e8cca8-e42c-11de-a347-f34a4f72eb7d

---
+++ core/disassembler_util.cc
@@ -0,0 +1,98 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "syzygy/core/disassembler_util.h"
+
+#include "base/basictypes.h"
+#include "mnemonics.h"  // NOLINT
+
+namespace core {
+
+bool IsNop(const _DInst& instruction) {
+  switch (instruction.opcode) {
+    default:
+      // Only the sequences recognized below qualify as NOP instructions.
+      return false;
+
+    case I_XCHG:
+      // This handles the 1 bytes NOP sequence.
+      //     1-byte: xchg eax, eax.
+      return instruction.ops[0].type == O_REG &&
+          instruction.ops[0].index == RM_AX &&
+          instruction.ops[1].type == O_REG &&
+          instruction.ops[1].index == RM_AX;
+
+    case I_NOP:
+      // This handles the 2, 4, 5, 7, 8 and 9 byte NOP sequences.
+      //     2-byte: 66 NOP
+      //     4-byte: NOP DWORD PTR [EAX + 0] (8-bit displacement)
+      //     5-byte: NOP DWORD PTR [EAX + EAX*1 + 0] (8-bit displacement)
+      //     7-byte: NOP DWORD PTR [EAX + 0] (32-bit displacement)
+      //     8-byte: NOP DWORD PTR [EAX + EAX*1 + 0] (32-bit displacement)
+      //     9-byte: NOP WORD PTR [EAX + EAX*1 + 0] (32-bit displacement)
+      return true;
+
+    case I_LEA:
+      // This handles the 3 and 6 byte NOP sequences.
+      //     3-byte: LEA REG, 0 (REG) (8-bit displacement)
+      //     6-byte: LEA REG, 0 (REG) (32-bit displacement)
+      return instruction.ops[0].type == O_REG &&
+          instruction.ops[1].type == O_SMEM &&
+          instruction.ops[0].index == instruction.ops[1].index &&
+          instruction.disp == 0;
+
+    case I_MOV:
+      // Not documented in the Intel manuals, but we see "mov reg, reg" a lot.
+      return instruction.ops[0].type == O_REG &&
+          instruction.ops[1].type == O_REG &&
+          instruction.ops[0].index == instruction.ops[1].index;
+  }
+}
+
+bool IsControlFlow(const _DInst& instruction) {
+  uint8 fc = META_GET_FC(instruction.meta);
+  if (fc == FC_CND_BRANCH || fc == FC_UNC_BRANCH ||
+      fc == FC_RET || fc == FC_SYS) {
+    return true;
+  }
+  return false;
+}
+
+bool IsImplicitControlFlow(const _DInst& instruction) {
+  uint8 fc = META_GET_FC(instruction.meta);
+  if (fc == FC_RET || fc == FC_SYS) {
+    // Control flow jumps implicitly out of the block.
+    return true;
+  } else if (fc == FC_CND_BRANCH || fc == FC_UNC_BRANCH) {
+    uint8 type = instruction.ops[0].type;
+    if (type == O_REG || type == O_MEM ||
+        type == O_SMEM || type == O_DISP) {
+        // There is an explicit branch, but the target is computed, stored
+        // in a register, or indirect. We don't follow those.
+        return true;
+    }
+  }
+  return false;
+}
+
+bool IsInterrupt(const _DInst& instruction) {
+  return META_GET_FC(instruction.meta) == FC_INT;
+}
+
+bool IsDebugInterrupt(const _DInst& instruction) {
+  return IsInterrupt(instruction) && instruction.size == 1 &&
+      instruction.opcode == I_INT_3;
+}
+
+}  // namespace core
