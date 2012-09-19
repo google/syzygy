46233696a10900681cc22ec46b239510fa9254f2
rogerm@chromium.org <rogerm@chromium.org@15e8cca8-e42c-11de-a347-f34a4f72eb7d>
2012-09-19 13:31:22 +0000

    Refactor CoverageGrinder to use grinder::basic_block_util.
    This CL modifies the CoverageGrinder to use the utilities
    found in grinder::basic_block_util. These utilities were
    hoisted out of the CoverageGrinder in a previous CL.
    
    See: https://codereview.appspot.com/6493120/
    
    R= chrisha@chromium.org, siggi@chromium.org
    
    BUG=
    
    Review URL: https://codereview.appspot.com/6495126
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk/syzygy@1134 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- grinder/coverage_grinder.h
+++ grinder/coverage_grinder.h
@@ -1,4 +1,4 @@
-// Copyright 2012 Google Inc.
+// Copyright 2012 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,19 +17,16 @@
 #ifndef SYZYGY_GRINDER_COVERAGE_GRINDER_H_
 #define SYZYGY_GRINDER_COVERAGE_GRINDER_H_
 
+#include "syzygy/grinder/basic_block_util.h"
 #include "syzygy/grinder/grinder.h"
-
 #include "syzygy/grinder/lcov_writer.h"
-#include "syzygy/grinder/line_info.h"
 
 namespace grinder {
 
+// This class processes trace files containing basic-block frequency data and
+// produces LCOV output.
 class CoverageGrinder : public GrinderInterface {
  public:
-  typedef core::RelativeAddress RelativeAddress;
-  typedef core::AddressRange<RelativeAddress, size_t> RelativeAddressRange;
-  typedef std::vector<RelativeAddressRange> RelativeAddressRangeVector;
-
   CoverageGrinder();
   ~CoverageGrinder();
 
@@ -41,7 +38,7 @@ class CoverageGrinder : public GrinderInterface {
   virtual bool OutputData(FILE* file) OVERRIDE;
   // @}
 
-  // @name ParseEventHandler overrides.
+  // @name BasicBlockFrequencyGrinder implementation.
   // @{
   virtual void OnBasicBlockFrequency(
       base::Time time,
@@ -51,33 +48,21 @@ class CoverageGrinder : public GrinderInterface {
   // @}
 
  protected:
-  struct PdbInfo {
-    // Line and coverage information for all the source files associated with
-    // a particular PDB.
-    LineInfo line_info;
-    // Basic-block addresses for the module associated with a particular PDB.
-    // Used to transform basic-block frequency data to line visits via
-    // line_info.
-    RelativeAddressRangeVector bb_ranges;
-  };
+  // Stores per-module coverage data, populated during calls to
+  // OnBasicBlockFrequency.
+  basic_block_util::PdbInfoMap pdb_info_cache_;
 
-  typedef std::map<std::wstring, PdbInfo> PdbInfoMap;
-
-  // Loads a new or retrieves the cached PDB info for the given PDB.
-  bool GetPdbInfo(const FilePath& pdb_path, PdbInfo** pdb_info);
+  // Stores the final coverage data, populated by Grind. Contains an aggregate
+  // of all LineInfo objects stored in the pdb_info_map_.
+  LcovWriter lcov_writer_;
 
   // Points to the parser that is feeding us events. Used to get module
   // information.
   Parser* parser_;
+
   // Set to true if any call to OnBasicBlockFrequency fails. Processing will
   // continue with a warning that results may be partial.
   bool event_handler_errored_;
-  // Stores per-module coverage data, populated during calls to
-  // OnBasicBlockFrequency.
-  PdbInfoMap pdb_info_map_;
-  // Stores the final coverage data, populated by Grind. Contains an aggregate
-  // of all LineInfo objects stored in the pdb_info_map_.
-  LcovWriter lcov_writer_;
 };
 
 }  // namespace grinder
