c4fefcdaff369897d3ba957aeae4d3e66665da40
Roger McFarlane <rogerm@chromium.org>
2012-10-24 17:20:07 +0000

    Add basic-block-entry mode to grinder.
    R= siggi@chromium.org
    
    Review URL: https://codereview.appspot.com/6762044
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@1180 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- syzygy/grinder/grinder_app.h
+++ syzygy/grinder/grinder_app.h
@@ -1,4 +1,4 @@
-// Copyright 2012 Google Inc.
+// Copyright 2012 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ class GrinderApp : public common::AppImplBase {
   enum Mode {
     kProfile,
     kCoverage,
+    kBasicBlockEntry,
   };
 
   // @name Implementation of the AppImplbase interface.
