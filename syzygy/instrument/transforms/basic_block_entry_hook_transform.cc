3d8f8460109e60021288b368f38c2bcb37a86c03
Etienne Bergeron <etienneb@chromium.org>
2013-05-07 14:13:00 +0000

    Fix off-by-one ID in the basic block entry count instrumentation. R=rogerm@chromium.org BUG=
    Review URL: https://codereview.appspot.com/9235043
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@1487 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- syzygy/instrument/transforms/basic_block_entry_hook_transform.cc
+++ syzygy/instrument/transforms/basic_block_entry_hook_transform.cc
@@ -391,25 +391,6 @@ bool BasicBlockEntryHookTransform::FindOrCreateThunk(
       name, BlockGraph::CODE_BLOCK, thunk_section_->id(), 1, 0);
   desc->basic_block_order.push_back(bb);
 
-  // We use the location/index in the bb_ranges vector of the current
-  // basic-block range as the basic_block_id, and we pass a pointer to
-  // the frequency data block as the module_data parameter. We then make
-  // a memory indirect call to the bb_entry_hook.
-  Immediate basic_block_id(bb_ranges_.size(), core::kSize32Bit);
-  Immediate module_data(add_frequency_data_.frequency_data_block(), 0);
-  Immediate original_function(Displacement(code_block, offset));
-  Operand bb_entry_hook(Displacement(bb_entry_hook_ref_.referenced(),
-                                     bb_entry_hook_ref_.offset()));
-
-  // Assemble entry hook instrumentation into the thunk's instruction stream.
-  // Note that we turn this into a simulated call, so that the return from
-  // the bb entry hook continues from the thunked function.
-  BasicBlockAssembler bb_asm(bb->instructions().begin(), &bb->instructions());
-  bb_asm.push(basic_block_id);
-  bb_asm.push(module_data);
-  bb_asm.push(original_function);
-  bb_asm.jmp(bb_entry_hook);
-
   // Find the source range associated with this block.
   BlockGraph::Block::SourceRange source_range;
   if (!code_block->source_ranges().empty())
@@ -423,6 +404,25 @@ bool BasicBlockEntryHookTransform::FindOrCreateThunk(
   // PostBlockGraphIteration function's check for overlapping source ranges.
   if (bb_ranges_.empty() || source_range != bb_ranges_.back())
     bb_ranges_.push_back(source_range);
+
+  // We use the location/index in the bb_ranges vector of the current
+  // basic-block range as the basic_block_id, and we pass a pointer to
+  // the frequency data block as the module_data parameter. We then make
+  // a memory indirect call to the bb_entry_hook.
+  Immediate basic_block_id(bb_ranges_.size()-1, core::kSize32Bit);
+  Immediate module_data(add_frequency_data_.frequency_data_block(), 0);
+  Immediate original_function(Displacement(code_block, offset));
+  Operand bb_entry_hook(Displacement(bb_entry_hook_ref_.referenced(),
+                                     bb_entry_hook_ref_.offset()));
+
+  // Assemble entry hook instrumentation into the thunk's instruction stream.
+  // Note that we turn this into a simulated call, so that the return from
+  // the bb entry hook continues from the thunked function.
+  BasicBlockAssembler bb_asm(bb->instructions().begin(), &bb->instructions());
+  bb_asm.push(basic_block_id);
+  bb_asm.push(module_data);
+  bb_asm.push(original_function);
+  bb_asm.jmp(bb_entry_hook);
 
   // Condense the whole mess into a block.
   BlockBuilder block_builder(block_graph);
