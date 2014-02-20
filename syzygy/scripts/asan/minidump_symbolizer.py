3676479ebb7595eeb9661c0c16f35b9a45b7b837
Sebastien Marchand <sebmarchand@chromium.org>
2014-02-20 15:56:11 +0000

    Improve ASan's symbolizer to support content_shell etc...
    The problem was that this script was specific to chrome.dll, now that we have a split build and that clusterfuzz also use content_shell.exe we need can't simply do a "reload /fi chrome.dll" to force the symbols of chrome.dll to be loaded as chrome.dll might not be in the modules list.
    
    Now I scan the loaded modules and the pdbs present in the |pdb_path| passed as an argument to the script and I load the appropriate symbols.
    
    R=chrisha@chromium.org
    
    BUG=
    
    Review URL: https://codereview.appspot.com/65780045
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@2041 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- syzygy/scripts/asan/minidump_symbolizer.py
+++ syzygy/scripts/asan/minidump_symbolizer.py
@@ -1,14 +1,14 @@
 #!python
 # Copyright 2013 Google Inc. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -24,24 +24,53 @@ import subprocess
 import sys
 
 
+# The sentinel value that we use at the end of the command executed in the
+# debugger.
 _SENTINEL = 'ENDENDEND'
-_DEFAULT_CDB_PATH = \
-    r'c:\Program Files (x86)\Debugging Tools for Windows (x86)\cdb.exe'
-_BAD_ACCESS_INFO_FRAME = "asan_rtl!agent::asan::AsanRuntime::OnError"
-_GET_BAD_ACCESS_INFO_COMMAND = "dt error_info"
-_GET_ALLOC_STACK_COMMAND = "dps @@(&error_info->alloc_stack) "  \
-                           "l@@(error_info->alloc_stack_size);"
-_GET_FREE_STACK_COMMAND = "dps @@(&error_info->free_stack) "  \
-                          "l@@(error_info->free_stack_size);"
-_ERROR_HELP_URL = "You can go to \
+
+
+# The default values for the path to cdb.exe.
+_DEFAULT_CDB_PATHS = [
+    r'c:\Program Files (x86)\Debugging Tools for Windows (x86)\cdb.exe',
+    r'c:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86\cdb.exe',
+  ]
+
+
+# The frame containing the error info structure.
+_BAD_ACCESS_INFO_FRAME = 'asan_rtl!agent::asan::AsanRuntime::OnError'
+
+
+# Various command that'll be run in the debugger.
+_GET_BAD_ACCESS_INFO_COMMAND = 'dt error_info'
+_GET_ALLOC_STACK_COMMAND = 'dps @@(&error_info->alloc_stack) ' \
+                           'l@@(error_info->alloc_stack_size);'
+_GET_FREE_STACK_COMMAND = 'dps @@(&error_info->free_stack) ' \
+                          'l@@(error_info->free_stack_size);'
+
+
+# The helper string that will be included at the beginning of the printed crash
+# reports.
+_ERROR_HELP_URL = 'You can go to \
 https://code.google.com/p/syzygy/wiki/SyzyASanBug to get more information \
-about how to treat this bug."
+about how to treat this bug.'
 
 
-ASanReport = namedtuple("ASanReport", "bad_access_info crash_stack alloc_stack "
-                        "free_stack")
+# A named tuple that will contain an ASan crash report.
+ASanReport = namedtuple('ASanReport', 'bad_access_info crash_stack alloc_stack '
+                        'free_stack')
 
 
+# Match a stack frame as printed by cdb.exe (or windbg.exe).
+#
+# Here's some examples of stack frames that this regex will match:
+#   - 003cd6b8 0ff3a36b 007bff00 00004e84 003cd760 foo!bar+0x18
+#   - 003cd6b8 0ff3a36b 007bff00 00004e84 003cd760 0xcafebabe
+#
+# Here's a description of the different groups in this regex:
+#     - args: The arguments in front of the module name.
+#     - module: The module's name.
+#     - location: The location in the module.
+#     - address: If the module name is not available then we'll get its address.
 _STACK_FRAME_RE = re.compile("""
     ^
     (?P<args>([0-9A-F]+\ +)+)
@@ -52,27 +81,61 @@ _STACK_FRAME_RE = re.compile("""
     $
     """, re.VERBOSE | re.IGNORECASE)
 
+
+# Match a list of modules as printed by cdb.exe when running the 'lm n' command.
+#
+# Here's a description of the different groups in this regex:
+#     - start: Module's start address.
+#     - end: Module's end address.
+#     - module_name: Module's name.
+#     - image_name: Image's name.
+_MODULE_MATCH_RE = re.compile("""
+    (?P<start>\w+)\s+
+    (?P<end>\w+)\s+
+    (?P<module_name>\w+)\s+
+    (?P<image_name>.*)
+    """, re.VERBOSE | re.IGNORECASE)
+
+
+# Match a Chrome frame in a stack trace.
 _CHROME_RE = re.compile('(chrome[_0-9A-F]+)', re.VERBOSE | re.IGNORECASE)
 
 
 def _Command(debugger, command):
+  """Execute a command in a debugger instance.
+
+  Args:
+    debugger: A handle to a cdb debugging session.
+    command: The command to execute.
+
+  Returns:
+    The output of the debugger after running this command.
+  """
   debugger.stdin.write(command + '; .echo %s\n' % _SENTINEL)
   lines = []
   while True:
     line = debugger.stdout.readline().rstrip()
-    # TODO(sebmarchand): Check for equality to avoid to stop if a line contains
-    #     the sentinel value.
-    if _SENTINEL in line:
+    # Sometime the sentinel value is preceded by '0:000> '.
+    if line.replace('0:000> ', '') == _SENTINEL:
       break
     lines.append(line)
   return lines
 
 
 def NormalizeChromeSymbol(symbol):
+  """Normalize a Chrome symbol."""
   return _CHROME_RE.sub('chrome_dll', symbol)
 
 
 def NormalizeStackTrace(stack_trace):
+  """Normalize a given stack trace.
+
+  Args:
+    stack_trace: The stack trace to normalize.
+
+  Returns:
+    The normalized stack trace.
+  """
   trace_hash = 0
   output_trace = []
   for line in stack_trace:
@@ -89,25 +152,59 @@ def NormalizeStackTrace(stack_trace):
       if location:
         location = NormalizeChromeSymbol(location)
       else:
-        location = "unknown"
+        location = 'unknown'
       frame = '%s!%s' % (module, location)
       output_trace.append(frame)
 
   return output_trace
 
 
+def LoadSymbols(debugger, pdb_path):
+  """Loads the pdbs for the loaded modules if they are present in |pdb_path|
+
+  Args:
+    debugger: A handle to a cdb debugging session.
+    command: The path containing the pdbs.
+  """
+  pdbs = [f for f in os.listdir(pdb_path) if f.endswith('.pdb')]
+  # The path needs to be quoted to avoid including the sentinel value in cdb's
+  # symbol search path.
+  _Command(debugger, '.sympath \"%s\"' % pdb_path)
+  for line in _Command(debugger, 'lm n'):
+    m = _MODULE_MATCH_RE.match(line)
+    if m is None:
+      continue
+    image_name =  m.group('image_name')
+    if image_name is None:
+      continue
+    pdb_name = image_name + '.pdb'
+    if pdb_name in pdbs:
+      _Command(debugger, '.reload /fi %s' % image_name)
+
+  _Command(debugger, '.symfix')
+
+
 def ProcessMinidump(minidump_filename, cdb_path, pdb_path):
+  """Process a minidump.
+
+  This analyzes the error contained in the minidump and returns the crash report
+  for it.
+
+  Args:
+    minidump_filename: The minidump filename.
+    cdb_path: The path to cdb.exe.
+    pdb_path: (Optional) The path to the pdbs for the loaded modules.
+
+  Returns:
+    The crash report to be printed.
+  """
   debugger = subprocess.Popen([cdb_path,
                                '-z', minidump_filename],
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
-
   if pdb_path is not None:
-    _Command(debugger, '.sympath %s' % pdb_path)
-    _Command(debugger, '.reload /fi chrome.dll')
-    _Command(debugger, '.reload /fi syzyasan_rtl.dll')
-    _Command(debugger, '.symfix')
+    LoadSymbols(debugger, pdb_path)
 
   # Enable the line number informations.
   _Command(debugger, '.lines')
@@ -128,7 +225,7 @@ def ProcessMinidump(minidump_filename, cdb_path, pdb_path):
     # End the debugging session.
     debugger.stdin.write('q\n')
     debugger.wait()
-    print "Unable to find the %s frame for %d." % (_BAD_ACCESS_INFO_FRAME,
+    print 'Unable to find the %s frame for %d.' % (_BAD_ACCESS_INFO_FRAME,
                                                    minidump_filename)
     return
 
@@ -155,19 +252,22 @@ def ProcessMinidump(minidump_filename, cdb_path, pdb_path):
 
 
 def PrintASanReport(report):
-  # Print the crash report.
+  """Print a crash report.
+
+  Args:
+    report: The report to print.
+  """
   print 'Bad access information:'
   for line in report.bad_access_info: print line
   print '\nCrash stack:'
   for line in report.crash_stack: print line
-  print '\nAllocation stack:'
-  for line in report.alloc_stack: print line
+  if len(report.alloc_stack) != 0:
+    print '\nAllocation stack:'
+    for line in report.alloc_stack: print line
   if len(report.free_stack) != 0:
     print '\nFree stack:'
     for line in report.free_stack: print line
   print '\n', _ERROR_HELP_URL
-
-  return
 
 
 _USAGE = """\
@@ -179,13 +279,16 @@ crash, alloc and free stack traces and gives more information about the crash.
 
 
 def _ParseArguments():
+  """Parse the command line arguments.
+
+  Returns:
+    The options on the command line.
+  """
   parser = optparse.OptionParser(usage=_USAGE)
   # TODO(sebmarchand): Move this to an argument instead of a switch?
   parser.add_option('--minidump',
                     help='The input minidump.')
-  parser.add_option('--cdb-path',
-                    default=_DEFAULT_CDB_PATH,
-                    help='(Optional) The path to cdb.exe.')
+  parser.add_option('--cdb-path', help='(Optional) The path to cdb.exe.')
   parser.add_option('--pdb-path',
                     help='(Optional) The path to the folder containing the'
                          ' PDBs.')
@@ -193,6 +296,14 @@ def _ParseArguments():
 
   if len(args):
     parser.error('Unexpected argument(s).')
+
+  if not opts.cdb_path:
+    for path in _DEFAULT_CDB_PATHS:
+      if os.path.isfile(path):
+        opts.cdb_path = path
+        break
+    if not opts.cdb_path:
+      parser.error('Unable to find cdb.exe.')
 
   if not opts.minidump:
     parser.error('You must provide a minidump.')
@@ -204,7 +315,6 @@ def _ParseArguments():
 
 def main():
   """Parse arguments and do the symbolization."""
-
   opts = _ParseArguments()
 
   report = ProcessMinidump(opts.minidump, opts.cdb_path, opts.pdb_path)
