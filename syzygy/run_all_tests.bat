5e815e8686b630a0078da7af4b010c61cb2005f0
Etienne Bergeron <etienneb@chromium.org>
2014-03-12 17:08:36 +0000

    Fix run_all_test script to avoid git-cl variable clashes.
    A recent update to git-cl seems to use the same variable
    SCRIPT, etc...
    
    After running run_all_tests, the git-cl script won't work until you unset
    the variables or close your shell.
    
    Unsetting the variable is enough, but it's safer to use a new name in
    case the script crashes.
    
    R=chrisha@chromium.org, chrisha
    
    BUG=
    
    Review URL: https://codereview.appspot.com/74770043
    
    git-svn-id: http://sawbuck.googlecode.com/svn/trunk@2072 15e8cca8-e42c-11de-a347-f34a4f72eb7d

--- syzygy/run_all_tests.bat
+++ syzygy/run_all_tests.bat
@@ -12,12 +12,16 @@
 :: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
-set PYTHON="%~dp0..\third_party\python_26\python.exe"
-set SCRIPT="%~dp0tests\run_all_tests.py"
-set INTERNAL_TESTS="%~dp0internal\run_all_tests.bat"
+set SYZYGY_PYTHON="%~dp0..\third_party\python_26\python.exe"
+set SYZYGY_SCRIPT="%~dp0tests\run_all_tests.py"
+set SYZYGY_INTERNAL_TESTS="%~dp0internal\run_all_tests.bat"
 
-%PYTHON% %SCRIPT% %*
+%SYZYGY_PYTHON% %SYZYGY_SCRIPT% %*
 
-IF EXIST %INTERNAL_TESTS% (
-%INTERNAL_TESTS% %*
+IF EXIST %SYZYGY_INTERNAL_TESTS% (
+%SYZYGY_INTERNAL_TESTS% %*
 )
+
+set SYZYGY_PYTHON=
+set SYZYGY_SCRIPT=
+set SYZYGY_INTERNAL_TESTS=
