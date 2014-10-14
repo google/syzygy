e4a08bde9442db85e71200d561d56ac08aff59be
Sebastien Marchand <sebmarchand@chromium.org>
2014-10-14 16:10:41 -0400

    Add support for the syzygy.gyp_env files.
    The file should be located in src/ (next to DEPS.syzygy) and called syzygy.gyp_env. It allows to overwrite some gyp variables.
    
    This is pretty much a copy paste from https://code.google.com/p/chromium/codesearch#chromium/src/build/gyp_helper.py&l=14
    BUG=
    R=erikwright@chromium.org
    
    Review URL: https://codereview.appspot.com/157060043

--- syzygy/build/gyp_main.py
+++ syzygy/build/gyp_main.py
@@ -20,10 +20,47 @@ import os
 import sys
 
 
+def apply_gyp_environment_from_file(file_path):
+  """Reads in a *.gyp_env file and applies the valid keys to os.environ."""
+  if not os.path.exists(file_path):
+    return
+  with open(file_path, 'rU') as f:
+    file_contents = f.read()
+  try:
+    file_data = eval(file_contents, {'__builtins__': None}, None)
+  except SyntaxError, e:
+    e.filename = os.path.abspath(file_path)
+    raise
+  supported_vars = (
+      'GYP_DEFINES',
+      'GYP_GENERATOR_FLAGS',
+      'GYP_GENERATORS',
+      'GYP_MSVS_VERSION',
+  )
+  for var in supported_vars:
+    file_val = file_data.get(var)
+    if file_val:
+      if var in os.environ:
+        print 'INFO: Environment value for "%s" overrides value in %s.' % (
+            var, os.path.abspath(file_path)
+        )
+      else:
+        os.environ[var] = file_val
+
+
+def apply_syzygy_gyp_env(syzygy_src_path):
+  if 'SKIP_SYZYGY_GYP_ENV' not in os.environ:
+    # Update the environment based on syzygy.gyp_env
+    path = os.path.join(syzygy_src_path, 'syzygy.gyp_env')
+    apply_gyp_environment_from_file(path)
+
+
 if __name__ == '__main__':
   # Get the path of the root 'src' directory.
   self_dir = os.path.abspath(os.path.dirname(__file__))
   src_dir = os.path.abspath(os.path.join(self_dir, '..', '..'))
+
+  apply_syzygy_gyp_env(src_dir)
 
   # Get the path to src/build. This contains a bunch of gyp
   # 'plugins' that get called by common.gypi and base.gyp.
