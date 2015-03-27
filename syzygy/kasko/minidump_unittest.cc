e1f61ff378a94cd083683c174a9c6e9612e9f2d1
Erik Wright <erikwright@chromium.org>
2015-03-27 10:44:09 -0400

    Update this test to take a dump of a child process.
    Other tests taking dumps of the main test process have been observed to hang. Although this test doesn't seem to have, it seems prudent to pre-emptively fix it.
    
    BUG=
    R=sebmarchand@chromium.org
    
    Review URL: https://codereview.appspot.com/217370043

--- syzygy/kasko/minidump_unittest.cc
+++ syzygy/kasko/minidump_unittest.cc
@@ -21,18 +21,43 @@
 #include <cstring>
 #include <vector>
 
+#include "base/base_switches.h"
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/macros.h"
 #include "base/files/file_path.h"
 #include "base/files/memory_mapped_file.h"
 #include "base/files/scoped_temp_dir.h"
+#include "base/process/kill.h"
+#include "base/process/launch.h"
+#include "base/strings/string16.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/synchronization/waitable_event.h"
+#include "base/test/multiprocess_test.h"
 #include "gtest/gtest.h"
 #include "syzygy/kasko/testing/minidump_unittest_helpers.h"
+#include "testing/multiprocess_func_list.h"
 
 namespace kasko {
 
 namespace {
+
+const char kReadyEventSwitch[] = "ready-event";
+
+// Signals an event named by kReadyEventSwitch, then blocks indefinitely.
+MULTIPROCESS_TEST_MAIN(MinidumpTestBlockingProcess) {
+  // Read the caller-supplied parameters.
+  base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
+  base::string16 ready_event_name =
+      base::ASCIIToUTF16(cmd_line->GetSwitchValueASCII(kReadyEventSwitch));
+  base::WaitableEvent ready_event(
+      ::OpenEvent(EVENT_MODIFY_STATE, FALSE, ready_event_name.c_str()));
+  ready_event.Signal();
+  ::Sleep(INFINITE);
+  return 0;
+}
 
 const char kCustomStreamContents[] = "hello world";
 uint32_t kCustomStreamType = LastReservedStream + 2468;
@@ -49,10 +74,39 @@ void ValidateMinidump(IDebugClient4* debug_client,
 class MinidumpTest : public ::testing::Test {
  public:
   MinidumpTest() {}
+
   ~MinidumpTest() override {}
 
   // ::testing::Test implementation.
   virtual void SetUp() override { temp_dir_.CreateUniqueTempDir(); }
+
+  void CallGenerateMinidump(const base::FilePath& dump_file_path,
+                            MinidumpType minidump_type,
+                            const std::vector<CustomStream>& custom_streams,
+                            bool* result) {
+    std::string ready_event_name =
+        "minidump_test_ready_" + base::UintToString(base::GetCurrentProcId());
+
+    base::WaitableEvent ready_event(::CreateEvent(
+        NULL, FALSE, FALSE, base::ASCIIToUTF16(ready_event_name).c_str()));
+
+    base::CommandLine child_command_line =
+        base::GetMultiProcessTestChildBaseCommandLine();
+    child_command_line.AppendSwitchASCII(switches::kTestChildProcess,
+                                         "MinidumpTestBlockingProcess");
+    child_command_line.AppendSwitchASCII(kReadyEventSwitch, ready_event_name);
+    base::ProcessHandle child_process;
+    ASSERT_TRUE(base::LaunchProcess(child_command_line, base::LaunchOptions(),
+                                    &child_process));
+    base::win::ScopedHandle scoped_child_process(child_process);
+    ready_event.Wait();
+
+    *result =
+        kasko::GenerateMinidump(dump_file_path, base::GetProcId(child_process),
+                                0, NULL, minidump_type, custom_streams);
+
+    ASSERT_TRUE(base::KillProcess(child_process, 0, true));
+  }
 
  protected:
   base::FilePath temp_dir() { return temp_dir_.path(); }
@@ -66,9 +120,10 @@ TEST_F(MinidumpTest, GenerateAndLoad) {
   // Generate a minidump for the current process.
   base::FilePath dump_file_path = temp_dir().Append(L"test.dump");
   std::vector<CustomStream> custom_streams;
-  ASSERT_TRUE(kasko::GenerateMinidump(dump_file_path, ::GetCurrentProcessId(),
-                                      0, NULL, SMALL_DUMP_TYPE,
-                                      custom_streams));
+  bool result = false;
+  ASSERT_NO_FATAL_FAILURE(CallGenerateMinidump(dump_file_path, SMALL_DUMP_TYPE,
+                                               custom_streams, &result));
+  ASSERT_TRUE(result);
 
   ASSERT_HRESULT_SUCCEEDED(
       testing::VisitMinidump(dump_file_path, base::Bind(&ValidateMinidump)));
@@ -82,9 +137,10 @@ TEST_F(MinidumpTest, CustomStream) {
                                 kCustomStreamContents,
                                 sizeof(kCustomStreamContents)};
   custom_streams.push_back(custom_stream);
-  ASSERT_TRUE(kasko::GenerateMinidump(dump_file_path, ::GetCurrentProcessId(),
-                                      0, NULL, SMALL_DUMP_TYPE,
-                                      custom_streams));
+  bool result = false;
+  ASSERT_NO_FATAL_FAILURE(CallGenerateMinidump(dump_file_path, SMALL_DUMP_TYPE,
+                                               custom_streams, &result));
+  ASSERT_TRUE(result);
 
   // Open the minidump file.
   base::MemoryMappedFile memory_mapped_file;
@@ -109,15 +165,18 @@ TEST_F(MinidumpTest, MinidumpType) {
   base::FilePath larger_dump_file_path = temp_dir().Append(L"larger.dump");
   base::FilePath full_dump_file_path = temp_dir().Append(L"full.dump");
   std::vector<CustomStream> custom_streams;
-  ASSERT_TRUE(kasko::GenerateMinidump(small_dump_file_path,
-                                      ::GetCurrentProcessId(), 0, NULL,
-                                      SMALL_DUMP_TYPE, custom_streams));
-  ASSERT_TRUE(kasko::GenerateMinidump(larger_dump_file_path,
-                                      ::GetCurrentProcessId(), 0, NULL,
-                                      LARGER_DUMP_TYPE, custom_streams));
-  ASSERT_TRUE(kasko::GenerateMinidump(full_dump_file_path,
-                                      ::GetCurrentProcessId(), 0, NULL,
-                                      FULL_DUMP_TYPE, custom_streams));
+
+
+  bool result = false;
+  ASSERT_NO_FATAL_FAILURE(CallGenerateMinidump(
+      small_dump_file_path, SMALL_DUMP_TYPE, custom_streams, &result));
+  ASSERT_TRUE(result);
+  ASSERT_NO_FATAL_FAILURE(CallGenerateMinidump(
+      larger_dump_file_path, LARGER_DUMP_TYPE, custom_streams, &result));
+  ASSERT_TRUE(result);
+  ASSERT_NO_FATAL_FAILURE(CallGenerateMinidump(
+      full_dump_file_path, FULL_DUMP_TYPE, custom_streams, &result));
+  ASSERT_TRUE(result);
 
   // Use the relative file sizes to infer that the correct minidump type was
   // respected.
@@ -142,9 +201,12 @@ TEST_F(MinidumpTest, OverwriteExistingFile) {
   base::FilePath dump_file_path;
   ASSERT_TRUE(base::CreateTemporaryFileInDir(temp_dir.path(), &dump_file_path));
   std::vector<CustomStream> custom_streams;
-  ASSERT_TRUE(kasko::GenerateMinidump(dump_file_path, ::GetCurrentProcessId(),
-                                      0, NULL, SMALL_DUMP_TYPE,
-                                      custom_streams));
+
+  bool result = false;
+  ASSERT_NO_FATAL_FAILURE(CallGenerateMinidump(dump_file_path, SMALL_DUMP_TYPE,
+                                               custom_streams, &result));
+  ASSERT_TRUE(result);
+
   ASSERT_HRESULT_SUCCEEDED(
       testing::VisitMinidump(dump_file_path, base::Bind(&ValidateMinidump)));
 }
@@ -153,9 +215,11 @@ TEST_F(MinidumpTest, NonexistantTargetDirectory) {
   base::ScopedTempDir temp_dir;
   ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
   std::vector<CustomStream> custom_streams;
-  ASSERT_FALSE(kasko::GenerateMinidump(
-      temp_dir.path().Append(L"Foobar").Append(L"HelloWorld"),
-      ::GetCurrentProcessId(), 0, NULL, SMALL_DUMP_TYPE, custom_streams));
+  bool result = false;
+  ASSERT_NO_FATAL_FAILURE(CallGenerateMinidump(
+      temp_dir.path().Append(L"Foobar").Append(L"HelloWorld"), SMALL_DUMP_TYPE,
+      custom_streams, &result));
+  ASSERT_FALSE(result);
 }
 
 }  // namespace kasko
