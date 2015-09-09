650bbd79f1ff5e59413a348bd3260ad4c34e5afe
Pierre-Antoine Manzagol <manzagop@chromium.org>
2015-09-09 10:40:40 -0400

    Refinery - Make StackAnalyzerTest reflective.
    StackAnalyzerTest now operates on a minidump of itself.
    
    BUG=
    R=siggi@chromium.org
    
    Review URL: https://codereview.chromium.org/1323423002.

--- syzygy/refinery/analyzers/stack_analyzer.cc
+++ syzygy/refinery/analyzers/stack_analyzer.cc
@@ -14,6 +14,7 @@
 
 #include "syzygy/refinery/analyzers/stack_analyzer.h"
 
+#include "base/strings/stringprintf.h"
 #include "syzygy/common/com_utils.h"
 #include "syzygy/refinery/analyzers/stack_analyzer_impl.h"
 #include "syzygy/refinery/process_state/refinery.pb.h"
@@ -41,16 +42,25 @@ Analyzer::AnalysisResult StackAnalyzer::Analyze(const Minidump& minidump,
 
   // Get the stack layer - it must already have been populated.
   StackLayerPtr stack_layer;
-  if (!process_state->FindLayer(&stack_layer))
+  if (!process_state->FindLayer(&stack_layer)) {
+    LOG(ERROR) << "Missing stack layer.";
     return ANALYSIS_ERROR;
-
-  // Process each thread's stack.
-  for (StackRecordPtr stack_record : *stack_layer) {
-    if (StackWalk(stack_record, process_state) == ANALYSIS_ERROR)
-      return ANALYSIS_ERROR;
   }
 
-  return ANALYSIS_COMPLETE;
+  // Process each thread's stack.
+  Analyzer::AnalysisResult result = ANALYSIS_COMPLETE;
+  for (StackRecordPtr stack_record : *stack_layer) {
+    // Attempt to stack walk. Note that the stack walk derailing is not an
+    // analysis error.
+    Analyzer::AnalysisResult stack_result =
+        StackWalk(stack_record, process_state);
+    if (stack_result == ANALYSIS_ERROR)
+      return ANALYSIS_ERROR;
+    if (stack_result == ANALYSIS_ITERATE)
+      result = ANALYSIS_ITERATE;
+  }
+
+  return result;
 }
 
 Analyzer::AnalysisResult StackAnalyzer::StackWalk(StackRecordPtr stack_record,
@@ -63,8 +73,11 @@ Analyzer::AnalysisResult StackAnalyzer::StackWalk(StackRecordPtr stack_record,
   HRESULT hr = stack_walker_->getEnumFrames(
       static_cast<IDiaStackWalkHelper*>(stack_walk_helper_.get()),
       frame_enumerator.Receive());
-  if (hr != S_OK)
+  if (hr != S_OK) {
+    LOG(ERROR) << "Failed to get frame enumerator: " << common::LogHr(hr)
+               << ".";
     return ANALYSIS_ERROR;
+  }
   frame_enumerator->Reset();
 
   // Walk the stack frames.
@@ -72,8 +85,11 @@ Analyzer::AnalysisResult StackAnalyzer::StackWalk(StackRecordPtr stack_record,
     base::win::ScopedComPtr<IDiaStackFrame> stack_frame;
     DWORD retrieved_cnt = 0;
     hr = frame_enumerator->Next(1, stack_frame.Receive(), &retrieved_cnt);
-    if (!SUCCEEDED(hr))
-      return ANALYSIS_ERROR;
+    if (!SUCCEEDED(hr)) {
+      // Stack walking derailed. Not an an analyzer error per se.
+      LOG(ERROR) << "Failed to get stack frame: " << common::LogHr(hr) << ".";
+      return ANALYSIS_COMPLETE;
+    }
     if (hr == S_FALSE || retrieved_cnt != 1)
       break;  // No frame.
 
@@ -82,13 +98,18 @@ Analyzer::AnalysisResult StackAnalyzer::StackWalk(StackRecordPtr stack_record,
     ULONGLONG frame_return_addr = 0ULL;
     if (stack_frame->get_size(&frame_size) != S_OK ||
         stack_frame->get_base(&frame_base) != S_OK ||
-        stack_frame->get_returnAddress(&frame_return_addr) != S_OK)
+        stack_frame->get_returnAddress(&frame_return_addr) != S_OK) {
+      LOG(ERROR) << "Failed to get stack_frame properties.";
       return ANALYSIS_ERROR;
+    }
 
     // TODO(manzagop): populate process state with stack frame information.
 
     if (frame_return_addr == 0ULL) {
       // WinDBG seems to use this as a termination criterion.
+      // TODO(manzagop): Remove. Temporary sentinel for stack walk success, used
+      // for unittests.
+      stack_record->mutable_data()->set_stack_walk_success(true);
       break;
     }
   }
--- syzygy/refinery/analyzers/stack_analyzer_unittest.cc
+++ syzygy/refinery/analyzers/stack_analyzer_unittest.cc
@@ -14,17 +14,28 @@
 
 #include "syzygy/refinery/analyzers/stack_analyzer.h"
 
+#include <Windows.h>  // NOLINT
+#include <DbgHelp.h>
+
 #include <string>
 
+#include "base/base_switches.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/multiprocess_test.h"
+#include "base/test/test_timeouts.h"
+#include "base/threading/platform_thread.h"
 #include "base/win/scoped_com_initializer.h"
 #include "gtest/gtest.h"
+#include "syzygy/common/com_utils.h"
 #include "syzygy/common/unittest_util.h"
 #include "syzygy/pe/unittest_util.h"
 #include "syzygy/refinery/unittest_util.h"
@@ -37,53 +48,225 @@
 #include "syzygy/refinery/process_state/process_state.h"
 #include "syzygy/refinery/process_state/process_state_util.h"
 #include "syzygy/refinery/process_state/refinery.pb.h"
+#include "testing/multiprocess_func_list.h"
 
 namespace refinery {
 
 namespace {
 
-// TODO(manzagop): factor out a testing::ScopedSetSymbolPath.
-const wchar_t kLocalSymbolPathSuffix[] = L"symbols\\microsoft";
+// Symbol path.
+const wchar_t kLocalSymbolDir[] = L"symbols";
 const char kNtSymbolPathPrefix[] = "SRV*";
-const char kNtSymbolPathSuffix[] =
+const char kNtSymbolPathSuffixMicrosoft[] =
     "*http://msdl.microsoft.com/download/symbols";
+const char kNtSymbolPathSuffixGoogle[] =
+    "*https://chromium-browser-symsrv.commondatastorage.googleapis.com";
+
+// Minidump.
+const wchar_t kMinidumpFileName[] = L"minidump.dmp";
+const char kSwitchExceptionPtrs[] = "exception-ptrs";
+const char kSwitchPid[] = "dump-pid";
+const char kSwitchMinidumpPath[] = "dump-path";
+const char kSwitchTid[] = "exception-thread-id";
+const MINIDUMP_TYPE kSmallDumpType = static_cast<MINIDUMP_TYPE>(
+    MiniDumpWithProcessThreadData |  // Get PEB and TEB.
+    MiniDumpWithUnloadedModules);    // Get unloaded modules when available.
+
+__declspec(noinline)
+DWORD GetEip() {
+  return reinterpret_cast<DWORD>(_ReturnAddress());
+}
+
+bool GetPathValueNarrow(const base::FilePath& path, std::string* value) {
+  const std::wstring value_wide = path.value();
+  return base::WideToUTF8(value_wide.c_str(), value_wide.length(), value);
+}
+
+bool GetNtSymbolPathValue(std::string* nt_symbol_path) {
+  DCHECK(nt_symbol_path);
+
+  base::FilePath output_path =
+      testing::GetOutputRelativePath(L"").NormalizePathSeparators();
+
+  // Build the local symbol directory path and ensure it exists.
+  base::FilePath local_symbol_path = output_path.Append(kLocalSymbolDir);
+  if (!base::CreateDirectory(local_symbol_path))
+    return false;
+
+  // Build the full symbol path.
+  std::string output_path_str;
+  if (!GetPathValueNarrow(output_path, &output_path_str))
+    return false;
+
+  std::string local_symbol_path_microsoft;
+  if (!GetPathValueNarrow(local_symbol_path.Append(L"microsoft"),
+                          &local_symbol_path_microsoft)) {
+    return false;
+  }
+  std::string local_symbol_path_google;
+  if (!GetPathValueNarrow(local_symbol_path.Append(L"google"),
+                          &local_symbol_path_google)) {
+    return false;
+  }
+
+  base::SStringPrintf(
+      nt_symbol_path, "%s;%s%s%s;%s%s%s", output_path_str.c_str(),
+      kNtSymbolPathPrefix, local_symbol_path_google.c_str(),
+      kNtSymbolPathSuffixGoogle, kNtSymbolPathPrefix,
+      local_symbol_path_microsoft.c_str(), kNtSymbolPathSuffixMicrosoft);
+
+  return true;
+}
 
 }  // namespace
+
+MULTIPROCESS_TEST_MAIN(MinidumpDumperProcess) {
+  // Retrieve information from the command line.
+  base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
+  if (!cmd_line->HasSwitch(kSwitchPid) || !cmd_line->HasSwitch(kSwitchTid) ||
+      !cmd_line->HasSwitch(kSwitchExceptionPtrs) ||
+      !cmd_line->HasSwitch(kSwitchMinidumpPath)) {
+    return 1;
+  }
+
+  std::string pid_string = cmd_line->GetSwitchValueASCII(kSwitchPid);
+  unsigned pid_uint = 0U;
+  if (!base::StringToUint(pid_string, &pid_uint))
+    return 1;
+  base::ProcessId pid = static_cast<base::ProcessId>(pid_uint);
+
+  std::string thread_id_string = cmd_line->GetSwitchValueASCII(kSwitchTid);
+  unsigned thread_id = 0U;
+  if (!base::StringToUint(thread_id_string, &thread_id))
+    return 1;
+
+  std::string exception_ptrs_string =
+      cmd_line->GetSwitchValueASCII(kSwitchExceptionPtrs);
+  unsigned exception_ptrs = 0ULL;
+  if (!base::StringToUint(exception_ptrs_string, &exception_ptrs))
+    return 1;
+
+  base::FilePath minidump_path =
+      cmd_line->GetSwitchValuePath(kSwitchMinidumpPath);
+
+  // Get handles to dumpee and dump file.
+  base::Process dumpee_process = base::Process::OpenWithAccess(
+      pid, PROCESS_QUERY_INFORMATION | PROCESS_VM_READ);
+  if (!dumpee_process.IsValid()) {
+    LOG(ERROR) << "Failed to open process: " << ::common::LogWe() << ".";
+    return 1;
+  }
+
+  base::File minidump_file(minidump_path,
+                           base::File::FLAG_CREATE | base::File::FLAG_WRITE);
+  if (!minidump_file.IsValid()) {
+    LOG(ERROR) << "Failed to create minidump file: " << minidump_path.value();
+    return 1;
+  }
+
+  // Build the dump related information.
+  MINIDUMP_EXCEPTION_INFORMATION exception_information = {};
+  exception_information.ThreadId = static_cast<DWORD>(thread_id);
+  exception_information.ExceptionPointers =
+      reinterpret_cast<PEXCEPTION_POINTERS>(exception_ptrs);
+  exception_information.ClientPointers = true;
+
+  MINIDUMP_USER_STREAM_INFORMATION* user_info = nullptr;
+  MINIDUMP_CALLBACK_INFORMATION* callback_info = nullptr;
+
+  // Take the minidump.
+  if (::MiniDumpWriteDump(dumpee_process.Handle(), pid,
+                          minidump_file.GetPlatformFile(), kSmallDumpType,
+                          &exception_information, user_info,
+                          callback_info) == FALSE) {
+    LOG(ERROR) << "MiniDumpWriteDump failed: " << ::common::LogWe() << ".";
+    return 1;
+  }
+
+  return 0;
+}
 
 class StackAnalyzerTest : public testing::Test {
  protected:
   void SetUp() override {
-    // Build the local symbol directory path and ensure it exists.
-    base::FilePath local_symbol_path =
-        testing::GetOutputRelativePath(kLocalSymbolPathSuffix);
-    ASSERT_TRUE(base::CreateDirectory(local_symbol_path));
-
-    // Build the full symbol path.
-    const std::wstring local_symbol_path_wide = local_symbol_path.value();
-    std::string local_symbol_path_narrow;
-    if (!base::WideToUTF8(local_symbol_path_wide.c_str(),
-                          local_symbol_path_wide.length(),
-                          &local_symbol_path_narrow)) {
-      FAIL();
-    }
-    std::string nt_symbol_path = base::StringPrintf(
-        "%s%s%s", kNtSymbolPathPrefix, local_symbol_path_narrow.c_str(),
-        kNtSymbolPathSuffix);
-
-    // Set the variable.
+    // Override NT symbol path.
+    std::string nt_symbol_path;
+    ASSERT_TRUE(GetNtSymbolPathValue(&nt_symbol_path));
     ASSERT_TRUE(
         scoped_env_variable_.Set(testing::kNtSymbolPathEnvVar, nt_symbol_path));
+
+    // Determine minidump path.
+    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    minidump_path_ = temp_dir_.path().Append(kMinidumpFileName);
+  }
+
+  base::FilePath temp_dir() { return temp_dir_.path(); }
+  base::FilePath minidump_path() { return minidump_path_; }
+
+  bool GenerateMinidump(CONTEXT* context) {
+    DCHECK(context);
+
+    // Build the exception information.
+    EXCEPTION_RECORD exception = {};
+    exception.ExceptionCode = 0xCAFEBABE;  // Note: a random error code.
+    exception.ExceptionAddress = reinterpret_cast<PVOID>(context->Eip);
+
+    EXCEPTION_POINTERS exception_pointers = {&exception, context};
+
+    // Build the dumper's command line.
+    base::CommandLine dumper_command_line(
+        base::GetMultiProcessTestChildBaseCommandLine());
+    dumper_command_line.AppendSwitchASCII(switches::kTestChildProcess,
+                                          "MinidumpDumperProcess");
+    base::Process current_process = base::Process::Current();
+    dumper_command_line.AppendSwitchASCII(
+        kSwitchPid, base::UintToString(current_process.Pid()));
+    dumper_command_line.AppendSwitchASCII(
+        kSwitchTid, base::UintToString(::GetCurrentThreadId()));
+    unsigned exception_pointers_uint =
+        reinterpret_cast<unsigned>(&exception_pointers);
+    dumper_command_line.AppendSwitchASCII(
+        kSwitchExceptionPtrs, base::UintToString(exception_pointers_uint));
+    dumper_command_line.AppendSwitchPath(kSwitchMinidumpPath, minidump_path());
+
+    // Launch the dumper.
+    base::Process dumper_process =
+        base::LaunchProcess(dumper_command_line, base::LaunchOptions());
+    int exit_code = 0;
+    bool success = dumper_process.WaitForExitWithTimeout(
+        TestTimeouts::action_timeout(), &exit_code);
+    if (!success) {
+      dumper_process.Terminate(0, true);
+      return false;
+    }
+
+    return exit_code == 0;
   }
 
  private:
+  base::ScopedTempDir temp_dir_;
+  base::FilePath minidump_path_;
+
   testing::ScopedEnvironmentVariable scoped_env_variable_;
 };
 
 TEST_F(StackAnalyzerTest, AnalyzeMinidump) {
   base::win::ScopedCOMInitializer com_initializer;
+  base::FilePath path;
+
+  // Generate a minidump.
+
+  // TODO(manzagop): set up some stack state.
+  CONTEXT context = {};
+  ::RtlCaptureContext(&context);
+  // RtlCaptureContext sets the instruction pointer to a value in this
+  // function's callee (similar to _ReturnAddress). Override it so the
+  // instruction pointer and the context actually match.
+  context.Eip = GetEip();
+  ASSERT_TRUE(GenerateMinidump(&context));
 
   Minidump minidump;
-  ASSERT_TRUE(minidump.Open(testing::TestMinidumps::GetNotepad32Dump()));
+  ASSERT_TRUE(minidump.Open(minidump_path()));
 
   // Analyze.
   ProcessState process_state;
@@ -104,7 +287,14 @@ TEST_F(StackAnalyzerTest, AnalyzeMinidump) {
   ASSERT_EQ(Analyzer::ANALYSIS_COMPLETE,
             runner.Analyze(minidump, &process_state));
 
-  // TODO(manzagop): validate process state for stack walking information.
+  // Ensure the test's thread was successfully walked.
+  // TODO(manzagop): actual validation of stack walk result once added to
+  // process state.
+  StackRecordPtr stack;
+  DWORD thread_id = ::GetCurrentThreadId();
+  ASSERT_TRUE(process_state.FindStackRecord(static_cast<size_t>(thread_id),
+                                            &stack));
+  ASSERT_TRUE(stack->data().stack_walk_success());
 }
 
 }  // namespace refinery
