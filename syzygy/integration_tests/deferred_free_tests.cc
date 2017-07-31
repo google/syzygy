ded3e13006dc2dacded62d04e2d68dde2cc2aa5b
njanevsk <njanevsk@google.com>
2017-07-31 08:42:40 -0700

    Add correct linker symbols fot tls for win64.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2989023003

--- syzygy/integration_tests/deferred_free_tests.cc
+++ syzygy/integration_tests/deferred_free_tests.cc
@@ -51,11 +51,27 @@ void NTAPI TlsCallback(PVOID, DWORD dwReason, PVOID) {
 }
 
 // Magic to set the TLS callback.
+#ifdef _WIN64
+#pragma comment(linker, "/INCLUDE:_tls_used")
+#pragma comment(linker, "/INCLUDE:tls_callback_func")
+#else
 #pragma comment(linker, "/INCLUDE:__tls_used")
 #pragma comment(linker, "/INCLUDE:_tls_callback_func")
+#endif
+
+#ifdef _WIN64
+#pragma const_seg(".CRT$XLF")
+#else
 #pragma data_seg(".CRT$XLF")
+#endif
+
 EXTERN_C PIMAGE_TLS_CALLBACK tls_callback_func = TlsCallback;
+
+#ifdef _WIN64
+#pragma const_seg()
+#else
 #pragma data_seg()
+#endif  //_WIN64
 
 }  // namespace
 
